c385e3668ac8db2d3a22a382a5637e758c9a1cfc
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-08 01:06:14 +0630

    -

--- DotNet8.Architectures.DTO/Features/Blog/BlogModel.cs
+++ DotNet8.Architectures.DTO/Features/Blog/BlogModel.cs
@@ -1,5 +1,7 @@
 ﻿namespace DotNet8.Architectures.DTO.Features.Blog;
 
+#region BlogModel
+
 public class BlogModel
 {
     public long BlogId { get; set; }
@@ -12,3 +14,5 @@ public class BlogModel
 
     public bool DeleteFlag { get; set; }
 }
+
+#endregion
