b35283506ef12c62b99c2055da8284ff632ef8b6
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-08 23:13:03 +0630

    -

--- DotNet8.Architectures.Utils/Enums/EnumStatusCode.cs
+++ DotNet8.Architectures.Utils/Enums/EnumStatusCode.cs
@@ -1,16 +1,15 @@
-﻿namespace DotNet8.Architectures.Utils.Enums
-{
-    public enum EnumStatusCode
-    {
-        None,
-        Success = 200,
-        Created = 201,
-        Accepted = 202,
-        BadRequest = 400,
-        NotFound = 404,
-        Conflict = 409,
-        Locked = 423,
-        InternalServerError = 500
+﻿namespace DotNet8.Architectures.Utils.Enums;
 
-    }
+public enum EnumStatusCode
+{
+    None,
+    Success = 200,
+    Created = 201,
+    Accepted = 202,
+    BadRequest = 400,
+    NotFound = 404,
+    Conflict = 409,
+    Locked = 423,
+    InternalServerError = 500
+
 }
