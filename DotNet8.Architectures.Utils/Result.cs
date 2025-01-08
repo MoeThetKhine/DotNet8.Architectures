f12279f312a0b5c6989127494d84ab4bb336864a
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-09 05:40:55 +0630

    Feat : Add Delete Success

--- DotNet8.Architectures.Utils/Result.cs
+++ DotNet8.Architectures.Utils/Result.cs
@@ -55,7 +55,15 @@ namespace DotNet8.Architectures.Utils
             };
         }
 
-      
-
+        public static Result<T> DeleteSuccess(string message = "Delete Successful.", EnumStatusCode statusCode = EnumStatusCode.Success)
+        {
+            return new Result<T>
+            {
+                IsSuccess = true,
+                Message = message,
+                StatusCode = statusCode
+            };
+        }
+       
     }
 }
