5b0a23638a6e5ba64ea8bed413d4d7e3f16b18a1
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-09 09:44:46 +0630

    Format Code

--- DotNet8.Architectures.DTO/Features/Blog/BlogRequestModel.cs
+++ DotNet8.Architectures.DTO/Features/Blog/BlogRequestModel.cs
@@ -1,11 +1,10 @@
-﻿namespace DotNet8.Architectures.DTO.Features.Blog
+﻿namespace DotNet8.Architectures.DTO.Features.Blog;
+
+public class BlogRequestModel
 {
-    public class BlogRequestModel
-    {
-        public string BlogTitle { get; set; } = null!;
+    public string BlogTitle { get; set; } = null!;
 
-        public string BlogAuthor { get; set; } = null!;
+    public string BlogAuthor { get; set; } = null!;
 
-        public string BlogContent { get; set; } = null!;
-    }
+    public string BlogContent { get; set; } = null!;
 }
