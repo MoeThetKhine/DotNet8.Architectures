9024ec241d076a95ab9b0f43ada8dfdc4d236836
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-09 09:49:43 +0630

    Feat : Add Blog Validator

--- DotNet8.Architectures.Shared/BlogValidator.cs
+++ DotNet8.Architectures.Shared/BlogValidator.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DotNet8.Architectures.DTO.Features.Blog;
+using FluentValidation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,21 @@ using System.Threading.Tasks;
 
 namespace DotNet8.Architectures.Shared
 {
-    internal class BlogValidator
+    public class BlogValidator : AbstractValidator<BlogRequestModel>
     {
+        public BlogValidator()
+        {
+            RuleFor(x => x.BlogTitle)
+                .NotEmpty().WithMessage("Blog Title cannot be empty.")
+                .NotNull().WithMessage("Blog Title cannot be null.");
+
+            RuleFor(x => x.BlogAuthor)
+                 .NotEmpty().WithMessage("Blog Author cannot be empty.")
+                .NotNull().WithMessage("Blog Author cannot be null.");
+
+            RuleFor(x => x.BlogContent)
+               .NotEmpty().WithMessage("Blog Content cannot be empty.")
+               .NotNull().WithMessage("Blog Content cannot be null.");
+        }
     }
 }
