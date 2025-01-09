ad7e7368fd40bce7f88e1a6a3efe925d330d8184
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-09 10:06:51 +0630

    Feat : Add BlogListModel

---
+++ DotNet8.Architectures.DTO/Features/Blog/BlogListModel.cs
@@ -0,0 +1,15 @@
+﻿using DotNet8.Architectures.DTO.PageSetting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet8.Architectures.DTO.Features.Blog
+{
+    public class BlogListModel
+    {
+        public IEnumerable<BlogModel> DataLst { get; set; }
+        public PageSettingModel PageSetting { get; set; }
+    }
+}
