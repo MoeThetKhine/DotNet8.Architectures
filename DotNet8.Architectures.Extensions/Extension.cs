6a20139a85fbf9b763b26325f558583900561104
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-10 15:01:00 +0630

    Feat : Add Blog Model

--- DotNet8.Architectures.Extensions/Extension.cs
+++ DotNet8.Architectures.Extensions/Extension.cs
@@ -1,5 +1,20 @@
-﻿namespace DotNet8.Architectures.Extensions;
+﻿using DotNet8.Architectures.DbServices.Models;
+using DotNet8.Architectures.DTO.Features.Blog;
+using System.Runtime.CompilerServices;
 
-internal class Extension
+namespace DotNet8.Architectures.Extensions;
+
+public static class Extension
 {
+    public static BlogModel ToModel(this TblBlog dataModel)
+    {
+        return new BlogModel
+        {
+            BlogId = dataModel.BlogId,
+            BlogTitle = dataModel.BlogTitle,
+            BlogAuthor = dataModel.BlogAuthor,
+            BlogContent = dataModel.BlogContent,
+            DeleteFlag = dataModel.DeleteFlag
+        };
+    }
 }
