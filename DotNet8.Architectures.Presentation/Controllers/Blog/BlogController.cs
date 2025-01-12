63471f0d30d748e625da007ac4ace4563f3b606e
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-13 00:39:37 +0630

    Format Code

--- DotNet8.Architectures.Presentation/Controllers/Blog/BlogController.cs
+++ DotNet8.Architectures.Presentation/Controllers/Blog/BlogController.cs
@@ -1,48 +1,47 @@
-﻿namespace DotNet8.Architectures.Presentation.Controllers.Blog
+﻿namespace DotNet8.Architectures.Presentation.Controllers.Blog;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BlogController : BaseController
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BlogController : BaseController
+    private readonly BL_Blog _bL_Blog;
+
+    public BlogController(BL_Blog bL_Blog)
     {
-        private readonly BL_Blog _bL_Blog;
+        _bL_Blog = bL_Blog;
+    }
+    [HttpGet]
+    public async Task<IActionResult> GetBlogAsync(int pageNo, int pageSize , CancellationToken cancellationToken)
+    {
+        var result = await _bL_Blog.GetBlogAsync(pageNo, pageSize, cancellationToken);
+        return Content(result);
+    }
 
-        public BlogController(BL_Blog bL_Blog)
-        {
-            _bL_Blog = bL_Blog;
-        }
-        [HttpGet]
-        public async Task<IActionResult> GetBlogAsync(int pageNo, int pageSize , CancellationToken cancellationToken)
-        {
-            var result = await _bL_Blog.GetBlogAsync(pageNo, pageSize, cancellationToken);
-            return Content(result);
-        }
+    [HttpPost]
+    public async Task<IActionResult> CreateBlogAsync([FromBody] BlogRequestModel blogRequest, CancellationToken cancellationToken)
+    {
+        var result = await _bL_Blog.AddBlogAsync(blogRequest, cancellationToken);
+        return Content(result);
+    }
 
-        [HttpPost]
-        public async Task<IActionResult> CreateBlogAsync([FromBody] BlogRequestModel blogRequest, CancellationToken cancellationToken)
-        {
-            var result = await _bL_Blog.AddBlogAsync(blogRequest, cancellationToken);
-            return Content(result);
-        }
+    [HttpPatch("{id}")]
+    public async Task<IActionResult> PatchBlogAsync([FromBody] int id , BlogRequestModel blogRequest, CancellationToken cancellationToken)
+    {
+        var result = await _bL_Blog.PatchBlogAsync(id, blogRequest, cancellationToken);
+        return Content(result);
+    }
 
-        [HttpPatch("{id}")]
-        public async Task<IActionResult> PatchBlogAsync([FromBody] int id , BlogRequestModel blogRequest, CancellationToken cancellationToken)
-        {
-            var result = await _bL_Blog.PatchBlogAsync(id, blogRequest, cancellationToken);
-            return Content(result);
-        }
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateBlogAsync([FromBody] int id , BlogRequestModel blogRequest , CancellationToken cancellationToken)
+    {
+        var result = await _bL_Blog.UpdateBlogAsync(id, blogRequest, cancellationToken); 
+        return Content(result);
+    }
 
-        [HttpPut("{id}")]
-        public async Task<IActionResult> UpdateBlogAsync([FromBody] int id , BlogRequestModel blogRequest , CancellationToken cancellationToken)
-        {
-            var result = await _bL_Blog.UpdateBlogAsync(id, blogRequest, cancellationToken); 
-            return Content(result);
-        }
-
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteBlogAsync(int id, CancellationToken cancellationToken)
-        {
-            var result = await _bL_Blog.DeleteBlogAsync(id, cancellationToken);
-            return Content(result);
-        }
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteBlogAsync(int id, CancellationToken cancellationToken)
+    {
+        var result = await _bL_Blog.DeleteBlogAsync(id, cancellationToken);
+        return Content(result);
     }
 }
