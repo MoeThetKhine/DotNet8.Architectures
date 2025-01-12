b0bf8bb76f6963e079762f7a8c14eda7ae596df1
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-12 14:46:26 +0630

    Feat : Add UpdateBlogAsync in BL_Blog

--- DotNet8.Architectures.BusinessLogic/Features/Blog/BL_Blog.cs
+++ DotNet8.Architectures.BusinessLogic/Features/Blog/BL_Blog.cs
@@ -93,7 +93,36 @@ namespace DotNet8.Architectures.BusinessLogic.Features.Blog
             }
         result:
             return response;
+        }
 
+        public async Task<Result<BlogModel>> UpdateBlogAsync(int id ,BlogRequestModel blogRequest, CancellationToken cancellationToken)
+        {
+            Result<BlogModel> response;
+
+            try
+            {
+                var validationResult = await _blogValidator.ValidateAsync(blogRequest);
+                if (!validationResult.IsValid)
+                {
+                    string errors = string.Join("", validationResult.Errors.Select(x => x.ErrorMessage));
+                    response = Result<BlogModel>.Fail(errors);
+                    goto result;
+                }
+
+                if(id <= 0)
+                {
+                    response = Result<BlogModel>.Fail(MessageResource.InvalidId);
+                    goto result;
+                }
+
+                response = await _dA_Blog.UpdateBlogAsync(id , blogRequest, cancellationToken);
+            }
+            catch (Exception ex)
+            {
+                response = Result<BlogModel>.Failure(ex);
+            }
+        result:
+            return response;
         }
 
     }
