2a7799e728a8ea738dfde3725eec92996d54b525
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-12 12:12:36 +0630

    Feat : Add DeleteBlogAsync in DA_blog

--- DotNet8.Architectures.DataAccess/Features/Blog/DA_Blog.cs
+++ DotNet8.Architectures.DataAccess/Features/Blog/DA_Blog.cs
@@ -210,5 +210,35 @@ namespace DotNet8.Architectures.DataAccess.Features.Blog
             return response;
         }
 
+        public async Task<Result<BlogModel>> DeleteBlogAsync(int id, CancellationToken cancellationToken)
+        {
+            Result<BlogModel> response;
+
+            try
+            {
+                var blog = await _context
+                    .TblBlogs
+                    .FirstOrDefaultAsync(x => x.BlogId == id, cancellationToken: cancellationToken);
+
+                if(blog is null)
+                {
+                    response = Result<BlogModel>.NotFound ();
+                    goto result;
+                }
+
+                _context.TblBlogs .Remove(blog);
+                await _context.SaveChangesAsync(cancellationToken);
+
+                response=Result<BlogModel>.UpdateSuccess();
+                    
+            }
+            catch(Exception ex)
+            {
+                response = Result<BlogModel>.Failure(ex);
+            }
+            result:
+            return response;
+        }
+
     }
 }
