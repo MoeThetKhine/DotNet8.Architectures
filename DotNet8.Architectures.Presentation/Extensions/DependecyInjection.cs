d71c48532bc3f615a5eb81cc4dd12e180382b96b
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-12 21:50:54 +0630

    Feat : Add AddDbContextService in DI

--- DotNet8.Architectures.Presentation/Extensions/DependecyInjection.cs
+++ DotNet8.Architectures.Presentation/Extensions/DependecyInjection.cs
@@ -1,6 +1,27 @@
-﻿namespace DotNet8.Architectures.Presentation.Extensions
+﻿using DotNet8.Architectures.BusinessLogic.Features.Blog;
+using DotNet8.Architectures.DataAccess.Features.Blog;
+using DotNet8.Architectures.DbServices.Models;
+using DotNet8.Architectures.Shared;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNet8.Architectures.Presentation.Extensions
 {
-    public class DependecyInjection
+    public static class DependecyInjection
     {
+       
+        private static IServiceCollection AddDbContextService(this IServiceCollection services, WebApplicationBuilder builder)
+        {
+            builder.Services.AddDbContext<AppDbContext>(opt =>
+            {
+                opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+                opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+            },
+            ServiceLifetime.Transient
+            );
+
+            return services;
+        }
+
+      
     }
 }
