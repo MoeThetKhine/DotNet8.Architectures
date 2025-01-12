126812872237c2a64a8b89e6714335d8aef23fa6
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-12 21:53:22 +0630

    Format Code

--- DotNet8.Architectures.Presentation/Extensions/DependecyInjection.cs
+++ DotNet8.Architectures.Presentation/Extensions/DependecyInjection.cs
@@ -4,47 +4,46 @@ using DotNet8.Architectures.DbServices.Models;
 using DotNet8.Architectures.Shared;
 using Microsoft.EntityFrameworkCore;
 
-namespace DotNet8.Architectures.Presentation.Extensions
+namespace DotNet8.Architectures.Presentation.Extensions;
+
+public static class DependecyInjection
 {
-    public static class DependecyInjection
+
+    public static IServiceCollection AddDependencyInjection(this IServiceCollection services, WebApplicationBuilder builder)
     {
+        return services
+            .AddDbContextService(builder)
+            .AddDataAccessService()
+            .AddBusinessLogicService()
+            .AddValidorService();
 
-        public static IServiceCollection AddDependencyInjection(this IServiceCollection services, WebApplicationBuilder builder)
+    }
+
+    private static IServiceCollection AddDbContextService(this IServiceCollection services, WebApplicationBuilder builder)
+    {
+        builder.Services.AddDbContext<AppDbContext>(opt =>
         {
-            return services
-                .AddDbContextService(builder)
-                .AddDataAccessService()
-                .AddBusinessLogicService()
-                .AddValidorService();
+            opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+        },
+        ServiceLifetime.Transient
+        );
 
-        }
+        return services;
+    }
 
-        private static IServiceCollection AddDbContextService(this IServiceCollection services, WebApplicationBuilder builder)
-        {
-            builder.Services.AddDbContext<AppDbContext>(opt =>
-            {
-                opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
-                opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
-            },
-            ServiceLifetime.Transient
-            );
+    private static IServiceCollection AddDataAccessService(this IServiceCollection services)
+    {
+        return services.AddScoped<DA_Blog>();
+    }
 
-            return services;
-        }
+    private static IServiceCollection AddBusinessLogicService(this IServiceCollection services)
+    {
+        return services.AddScoped<BL_Blog>();
+    }
 
-        private static IServiceCollection AddDataAccessService(this IServiceCollection services)
-        {
-            return services.AddScoped<DA_Blog>();
-        }
-
-        private static IServiceCollection AddBusinessLogicService(this IServiceCollection services)
-        {
-            return services.AddScoped<BL_Blog>();
-        }
-
-        private static IServiceCollection AddValidorService(this IServiceCollection services)
-        {
-            return services.AddScoped<BlogValidator>();
-        }
+    private static IServiceCollection AddValidorService(this IServiceCollection services)
+    {
+        return services.AddScoped<BlogValidator>();
     }
 }
