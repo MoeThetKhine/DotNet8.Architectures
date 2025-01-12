af1c9427887f8a45aed8f1e3b86da7eda06f6274
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-12 12:29:23 +0630

    Feat : Add Global Using

--- DotNet8.Architectures.DataAccess/GlobalUsing.cs
+++ DotNet8.Architectures.DataAccess/GlobalUsing.cs
@@ -1,1 +1,7 @@
-﻿
+﻿global using DotNet8.Architectures.DbServices.Models;
+global using DotNet8.Architectures.DTO.Features.Blog;
+global using DotNet8.Architectures.DTO.PageSetting;
+global using DotNet8.Architectures.Extensions;
+global using DotNet8.Architectures.Shared;
+global using DotNet8.Architectures.Utils;
+global using Microsoft.EntityFrameworkCore;
