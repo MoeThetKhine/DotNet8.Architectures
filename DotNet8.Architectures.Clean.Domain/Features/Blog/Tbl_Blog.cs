12420a32d93c76e68881854d1b6f11052f83ec48
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-13 23:01:18 +0630

    Format Code

--- DotNet8.Architectures.Clean.Domain/Features/Blog/Tbl_Blog.cs
+++ DotNet8.Architectures.Clean.Domain/Features/Blog/Tbl_Blog.cs
@@ -1,12 +1,11 @@
-﻿namespace DotNet8.Architectures.Clean.Domain.Features.Blog
+﻿namespace DotNet8.Architectures.Clean.Domain.Features.Blog;
+
+[Table("Tbl_Blog")]
+public class Tbl_Blog
 {
-    [Table("Tbl_Blog")]
-    public class Tbl_Blog
-    {
-        [Key]
-        public int BlogId { get; set; }
-        public string BlogTitle { get; set; }
-        public string BlogAuthor { get; set; }
-        public string BlogContent { get; set; }
-    }
+    [Key]
+    public int BlogId { get; set; }
+    public string BlogTitle { get; set; }
+    public string BlogAuthor { get; set; }
+    public string BlogContent { get; set; }
 }
