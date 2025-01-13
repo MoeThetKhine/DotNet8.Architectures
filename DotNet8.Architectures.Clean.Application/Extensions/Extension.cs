2189c104c686232fd7412f29f846052d5cd96a43
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-13 23:41:54 +0630

    -

--- DotNet8.Architectures.Clean.Application/Extensions/Extension.cs
+++ DotNet8.Architectures.Clean.Application/Extensions/Extension.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DotNet8.Architectures.Clean.Application.Extensions
+﻿namespace DotNet8.Architectures.Clean.Application.Extensions
 {
     internal class Extension
     {
