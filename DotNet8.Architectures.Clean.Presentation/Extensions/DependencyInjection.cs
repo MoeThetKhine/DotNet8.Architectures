283c8bd121b189e2e62a9a9e19cdfd0aa6cac40a
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-14 01:20:04 +0630

    Feat : Add AddRepositoryService in DI

--- DotNet8.Architectures.Clean.Presentation/Extensions/DependencyInjection.cs
+++ DotNet8.Architectures.Clean.Presentation/Extensions/DependencyInjection.cs
@@ -1,6 +1,13 @@
-﻿namespace DotNet8.Architectures.Clean.Presentation.Extensions
+﻿using DotNet8.Architectures.Clean.Domain.Features.Blog;
+using DotNet8.Architectures.Clean.Infrastructure.Features.Blog;
+
+namespace DotNet8.Architectures.Clean.Presentation.Extensions
 {
-    public class DependencyInjection
+    public static class DependencyInjection
     {
+        private static IServiceCollection AddRepositoryService(this IServiceCollection services)
+        {
+            return services.AddScoped<IBlogRepository, BlogRepository>();
+        }
     }
 }
