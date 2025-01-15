553ba7fb35112bd4949fde9041e6b0be420fe381
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-16 00:03:41 +0630

    Feat : Add Global Using

--- DotNet8.Architectures.Clean.Infrastructure/GlobalUsing.cs
+++ DotNet8.Architectures.Clean.Infrastructure/GlobalUsing.cs
@@ -1,1 +1,6 @@
-﻿
+﻿global using DotNet8.Architectures.Clean.Domain.Features.Blog;
+global using DotNet8.Architectures.DTO.Features.Blog;
+global using DotNet8.Architectures.DTO.PageSetting;
+global using DotNet8.Architectures.Utils;
+global using Microsoft.EntityFrameworkCore;
+global using DotNet8.Architectures.Shared;
