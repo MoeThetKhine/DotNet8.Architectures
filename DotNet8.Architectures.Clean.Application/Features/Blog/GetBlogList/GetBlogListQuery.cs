ecb8cd256c7fcc90bf1e781e23c488c198f85e3a
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-16 00:36:46 +0630

    -

--- DotNet8.Architectures.Clean.Application/Features/Blog/GetBlogList/GetBlogListQuery.cs
+++ DotNet8.Architectures.Clean.Application/Features/Blog/GetBlogList/GetBlogListQuery.cs
@@ -5,9 +5,14 @@ public class GetBlogListQuery : IRequest<Result<BlogListModelV1>>
     public int PageNo { get; set; }
     public int PageSize { get; set; }
 
+    #region GetBlogListQuery
+
     public GetBlogListQuery(int pageNo, int pageSize)
     {
         PageNo = pageNo;
         PageSize = pageSize;
     }
+
+    #endregion
+
 }
