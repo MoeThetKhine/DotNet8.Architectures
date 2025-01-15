6885c853b9fd096efc9ad8aa857572dfb12fa92a
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-16 00:01:40 +0630

    Format Code

--- DotNet8.Architectures.Clean.Infrastructure/Features/Blog/BlogRepository.cs
+++ DotNet8.Architectures.Clean.Infrastructure/Features/Blog/BlogRepository.cs
@@ -5,55 +5,54 @@ using DotNet8.Architectures.Utils;
 using Microsoft.EntityFrameworkCore;
 using DotNet8.Architectures.Shared;
 
-namespace DotNet8.Architectures.Clean.Infrastructure.Features.Blog
+namespace DotNet8.Architectures.Clean.Infrastructure.Features.Blog;
+
+public class BlogRepository : IBlogRepository
 {
-    public class BlogRepository : IBlogRepository
+    private readonly BlogDbContext _context;
+
+    public BlogRepository(BlogDbContext context)
     {
-        private readonly BlogDbContext _context;
+        _context = context;
+    }
 
-        public BlogRepository(BlogDbContext context)
+    public async Task<Result<BlogListModelV1>> GetBlogsAsync(int pageNo,int pageSize,CancellationToken cancellationToken)
+    {
+        Result<BlogListModelV1> result;
+        try
         {
-            _context = context;
-        }
-
-        public async Task<Result<BlogListModelV1>> GetBlogsAsync(int pageNo,int pageSize,CancellationToken cancellationToken)
-        {
-            Result<BlogListModelV1> result;
-            try
+            var query = _context.Tbl_Blogs.OrderByDescending(x => x.BlogId);
+            var lst = await query
+                .Paginate(pageNo, pageSize)
+                .ToListAsync(cancellationToken: cancellationToken);
+            var totalCount = await query.CountAsync(cancellationToken: cancellationToken);
+            var pageCount = totalCount / pageSize;
+            if (totalCount % pageSize > 0)
             {
-                var query = _context.Tbl_Blogs.OrderByDescending(x => x.BlogId);
-                var lst = await query
-                    .Paginate(pageNo, pageSize)
-                    .ToListAsync(cancellationToken: cancellationToken);
-                var totalCount = await query.CountAsync(cancellationToken: cancellationToken);
-                var pageCount = totalCount / pageSize;
-                if (totalCount % pageSize > 0)
-                {
-                    pageCount++;
-                }
-
-                var pageSettingModel = new PageSettingModel(pageNo, pageSize, pageCount, totalCount);
-                var model = new BlogListModelV1()
-                {
-                    DataLst = lst.Select(x => new BlogModel()
-                    {
-                        BlogId = x.BlogId,
-                        BlogTitle = x.BlogTitle,
-                        BlogAuthor = x.BlogAuthor,
-                        BlogContent = x.BlogContent
-                    })
-                        .AsQueryable(),
-                    PageSetting = pageSettingModel
-                };
-
-                result = Result<BlogListModelV1>.Success(model);
-            }
-            catch (Exception ex)
-            {
-                result = Result<BlogListModelV1>.Failure(ex);
+                pageCount++;
             }
 
-            return result;
+            var pageSettingModel = new PageSettingModel(pageNo, pageSize, pageCount, totalCount);
+            var model = new BlogListModelV1()
+            {
+                DataLst = lst.Select(x => new BlogModel()
+                {
+                    BlogId = x.BlogId,
+                    BlogTitle = x.BlogTitle,
+                    BlogAuthor = x.BlogAuthor,
+                    BlogContent = x.BlogContent
+                })
+                    .AsQueryable(),
+                PageSetting = pageSettingModel
+            };
+
+            result = Result<BlogListModelV1>.Success(model);
         }
+        catch (Exception ex)
+        {
+            result = Result<BlogListModelV1>.Failure(ex);
+        }
+
+        return result;
     }
 }
