03ac06b622b54c7211315c4d3456292f64c41271
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-16 00:01:23 +0630

    -

--- DotNet8.Architectures.Clean.Infrastructure/Features/Blog/BlogRepository.cs
+++ DotNet8.Architectures.Clean.Infrastructure/Features/Blog/BlogRepository.cs
@@ -3,11 +3,6 @@ using DotNet8.Architectures.DTO.Features.Blog;
 using DotNet8.Architectures.DTO.PageSetting;
 using DotNet8.Architectures.Utils;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using DotNet8.Architectures.Shared;
 
 namespace DotNet8.Architectures.Clean.Infrastructure.Features.Blog
