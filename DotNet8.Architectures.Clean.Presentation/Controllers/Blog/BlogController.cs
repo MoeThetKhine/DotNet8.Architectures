97d43f0760d96e11568ac333a644d325f58024b1
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-16 00:31:58 +0630

    Format Code

--- DotNet8.Architectures.Clean.Presentation/Controllers/Blog/BlogController.cs
+++ DotNet8.Architectures.Clean.Presentation/Controllers/Blog/BlogController.cs
@@ -1,23 +1,22 @@
-﻿namespace DotNet8.Architectures.Clean.Presentation.Controllers.Blog
+﻿namespace DotNet8.Architectures.Clean.Presentation.Controllers.Blog;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BlogController : BaseController
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BlogController : BaseController
+    private readonly IMediator _mediator;
+
+    public BlogController(IMediator mediator)
     {
-        private readonly IMediator _mediator;
+        _mediator = mediator;
+    }
 
-        public BlogController(IMediator mediator)
-        {
-            _mediator = mediator;
-        }
+    [HttpGet]
+    public async Task<IActionResult> GetBlogs(int pageNo,int pageSize,CancellationToken cancellationToken)
+    {
+        var query = new GetBlogListQuery(pageNo, pageSize);
+        var result = await _mediator.Send(query, cancellationToken);
 
-        [HttpGet]
-        public async Task<IActionResult> GetBlogs(int pageNo,int pageSize,CancellationToken cancellationToken)
-        {
-            var query = new GetBlogListQuery(pageNo, pageSize);
-            var result = await _mediator.Send(query, cancellationToken);
-
-            return Content(result);
-        }
+        return Content(result);
     }
 }
