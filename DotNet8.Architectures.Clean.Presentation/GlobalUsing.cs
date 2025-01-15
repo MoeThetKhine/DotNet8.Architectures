1238a59a336bbba06b7b0be987c59070bfe8733e
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-16 00:31:40 +0630

    Feat : Add Global Using

--- DotNet8.Architectures.Clean.Presentation/GlobalUsing.cs
+++ DotNet8.Architectures.Clean.Presentation/GlobalUsing.cs
@@ -1,3 +1,5 @@
 ﻿global using DotNet8.Architectures.Clean.Domain.Features.Blog;
 global using DotNet8.Architectures.Shared;
 global using Microsoft.AspNetCore.Mvc;
+global using DotNet8.Architectures.Clean.Application.Features.Blog.GetBlogList;
+global using MediatR;
