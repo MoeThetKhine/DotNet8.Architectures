923067929938910efdea58d11662ee1011b47181
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-15 23:46:42 +0630

    Feat : Create BlogRepository

--- DotNet8.Architectures.Clean.Infrastructure/Features/Blog/BlogRepository.cs
+++ DotNet8.Architectures.Clean.Infrastructure/Features/Blog/BlogRepository.cs
@@ -1,57 +1,12 @@
-﻿namespace DotNet8.Architectures.Clean.Infrastructure.Features.Blog;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class BlogRepository : IBlogRepository
+namespace DotNet8.Architectures.Clean.Infrastructure.Features.Blog
 {
-    private readonly BlogDbContext _context;
-
-    public BlogRepository(BlogDbContext context)
+    internal class BlogRepository
     {
-        _context = context;
     }
-
-    #region GetBlogsAsync
-
-    public async Task<Result<BlogListModelV1>> GetBlogsAsync(int pageNo, int pageSize, CancellationToken cancellationToken)
-    {
-        Result<BlogListModelV1> result;
-
-        try
-        {
-            var query = _context.Tbl_Blogs.OrderByDescending(x => x.BlogId);
-            var lst = await query.Paginate(pageNo, pageSize)
-                                 .ToListAsync(cancellationToken : cancellationToken);
-            
-            var totalCount = await query.CountAsync(cancellationToken : cancellationToken);
-            var pageCount = totalCount / pageSize;
-
-            if(totalCount % pageSize == 0)
-            {
-                pageCount++;
-            }
-
-            var pageSettingModel = new PageSettingModel(pageNo, pageSize, pageCount, totalCount);
-            var model = new BlogListModelV1()
-            {
-                DataLst = lst.Select(x => new BlogModel()
-                {
-                    BlogId = x.BlogId,
-                    BlogTitle = x.BlogTitle,
-                    BlogAuthor = x.BlogAuthor,
-                    BlogContent = x.BlogContent
-                })
-                    .AsQueryable(),
-                PageSetting = pageSettingModel
-            };
-            result = Result<BlogListModelV1>.Success(model);
-        }
-        catch (Exception ex)
-        {
-            result = Result<BlogListModelV1>.Failure(ex);
-        }
-
-        return result;
-    }
-
-    #endregion
-
 }
