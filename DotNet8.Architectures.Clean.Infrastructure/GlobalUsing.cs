762067f7f602f164d4d99262b8e9a241caa87ae9
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-15 23:46:50 +0630

    -

--- DotNet8.Architectures.Clean.Infrastructure/GlobalUsing.cs
+++ DotNet8.Architectures.Clean.Infrastructure/GlobalUsing.cs
@@ -1,6 +1,1 @@
-﻿global using DotNet8.Architectures.DTO.Features.Blog;
-global using DotNet8.Architectures.Utils;
-global using DotNet8.Architectures.Shared;
-global using Microsoft.EntityFrameworkCore;
-global using DotNet8.Architectures.DTO.PageSetting;
-global using DotNet8.Architectures.Clean.Domain.Features.Blog;
+﻿
