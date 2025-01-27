719b0161a94ea3321fdb014d98e6cdb80f0c709b
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-27 16:20:02 +0630

    -

--- DotNet8.Architectures.Clean.Application/Extensions/Extension.cs
+++ DotNet8.Architectures.Clean.Application/Extensions/Extension.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DotNet8.Architectures.Clean.Application.Extensions
 {
-	internal class Extension
+	public static IServiceCollection AddMediatRService(this IServiceCollection services)
 	{
 	}
 }
