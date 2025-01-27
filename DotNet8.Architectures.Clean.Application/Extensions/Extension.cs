e9f57202a6bdce382dd3399c530469126d35295c
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-27 16:30:51 +0630

    -

--- DotNet8.Architectures.Clean.Application/Extensions/Extension.cs
+++ DotNet8.Architectures.Clean.Application/Extensions/Extension.cs
@@ -2,6 +2,8 @@
 
 namespace DotNet8.Architectures.Clean.Application.Extensions;
 
+#region Extension
+
 public static class Extension
 {
 	public static IServiceCollection AddMediatRService(this IServiceCollection services)
@@ -10,3 +12,5 @@ public static class Extension
 		cf.RegisterServicesFromAssembly(typeof(Extension).Assembly));
 	}
 }
+
+#endregion
