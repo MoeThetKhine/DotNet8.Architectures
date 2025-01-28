524b2d15390680603caac6ba23b1ccf0dc6f5d7f
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-28 15:23:48 +0630

    Feat : Add global using

--- DotNet8.Architectures.Clean.Presentation/GlobalUsing.cs
+++ DotNet8.Architectures.Clean.Presentation/GlobalUsing.cs
@@ -2,3 +2,5 @@
 global using DotNet8.Architectures.Clean.Domain.Features.Blog;
 global using DotNet8.Architectures.Clean.Infrastructure.Features.Blog;
 global using Microsoft.EntityFrameworkCore;
+global using DotNet8.Architectures.Clean.Application.Extensions;
+global using DotNet8.Architectures.Clean.Presentation.Extensions;
