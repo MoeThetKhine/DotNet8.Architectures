4e473593a739b947084daa22ea41258b092bbe96
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-28 10:48:17 +0630

    Feat : Add BlogDbContext

--- DotNet8.Architectures.Clean.Infrastructure/Features/Blog/BlogDbContext.cs
+++ DotNet8.Architectures.Clean.Infrastructure/Features/Blog/BlogDbContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DotNet8.Architectures.DbServices.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,10 @@ using System.Threading.Tasks;
 
 namespace DotNet8.Architectures.Clean.Infrastructure.Features.Blog
 {
-	internal class BlogDbContext
+	public class BlogDbContext : DbContext
 	{
+		public BlogDbContext(DbContextOptions options) : base(options) { }
+
+		public DbSet<TblBlog> Tbl_Blogs { get; set; }
 	}
 }
