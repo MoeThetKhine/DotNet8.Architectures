66b5eb457d0a3a91a1abd799059cb1dc87b58cd5
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-28 11:03:44 +0630

    -

--- DotNet8.Architectures.Clean.Infrastructure/Features/Blog/BlogDbContext.cs
+++ DotNet8.Architectures.Clean.Infrastructure/Features/Blog/BlogDbContext.cs
@@ -1,12 +1,11 @@
 ﻿using DotNet8.Architectures.DbServices.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace DotNet8.Architectures.Clean.Infrastructure.Features.Blog
-{
-	public class BlogDbContext : DbContext
-	{
-		public BlogDbContext(DbContextOptions options) : base(options) { }
+namespace DotNet8.Architectures.Clean.Infrastructure.Features.Blog;
 
-		public DbSet<TblBlog> Tbl_Blogs { get; set; }
-	}
+public class BlogDbContext : DbContext
+{
+	public BlogDbContext(DbContextOptions options) : base(options) { }
+
+	public DbSet<TblBlog> Tbl_Blogs { get; set; }
 }
