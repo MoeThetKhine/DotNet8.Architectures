00bfd66beb38b7501be381390f72bd41d794bf4a
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-28 11:29:39 +0630

    Format Code

--- DotNet8.Architectures.Clean.Presentation/Controllers/BaseController.cs
+++ DotNet8.Architectures.Clean.Presentation/Controllers/BaseController.cs
@@ -1,10 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿namespace DotNet8.Architectures.Clean.Presentation.Controllers;
 
-namespace DotNet8.Architectures.Clean.Presentation.Controllers
+[Route("api/[controller]")]
+[ApiController]
+public class BaseController : ControllerBase
 {
-	[Route("api/[controller]")]
-	[ApiController]
-	public class BaseController : ControllerBase
-	{
-	}
 }
