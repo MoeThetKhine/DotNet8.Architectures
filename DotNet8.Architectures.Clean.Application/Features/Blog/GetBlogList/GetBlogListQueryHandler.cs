7f97f47dd43ba05b87a9c5c93da26ca04029529c
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-28 14:28:10 +0630

    Format Code

--- DotNet8.Architectures.Clean.Application/Features/Blog/GetBlogList/GetBlogListQueryHandler.cs
+++ DotNet8.Architectures.Clean.Application/Features/Blog/GetBlogList/GetBlogListQueryHandler.cs
@@ -1,35 +1,34 @@
-﻿namespace DotNet8.Architectures.Clean.Application.Features.Blog.GetBlogList
+﻿namespace DotNet8.Architectures.Clean.Application.Features.Blog.GetBlogList;
+
+public class GetBlogListQueryHandler : IRequestHandler<GetBlogListQuery , Result<BlogListModelV1>>
 {
-	public class GetBlogListQueryHandler : IRequestHandler<GetBlogListQuery , Result<BlogListModelV1>>
+	private readonly IBlogRepository _blogRepository;
+
+	public GetBlogListQueryHandler(IBlogRepository blogRepository)
 	{
-		private readonly IBlogRepository _blogRepository;
-
-		public GetBlogListQueryHandler(IBlogRepository blogRepository)
-		{
-			_blogRepository = blogRepository;
-		}
-
-		public async Task<Result<BlogListModelV1>> Handle (GetBlogListQuery request, CancellationToken cancellationToken)
-		{
-			Result<BlogListModelV1> result;
-
-			if(request.PageNo <= 0)
-			{
-				result = Result<BlogListModelV1>.Fail(MessageResource.InvalidPageNo);
-				goto result;
-			}
-
-			if (request.PageSize <= 0)
-			{
-				result = Result<BlogListModelV1>.Fail(MessageResource.InvalidPageSize);
-				goto result;
-			}
-
-			result = await _blogRepository.GetBlogsAsync(request.PageNo, request.PageSize, cancellationToken);
-
-			result:
-			return result;
-		}
+		_blogRepository = blogRepository;
 	}
-	
+
+	public async Task<Result<BlogListModelV1>> Handle (GetBlogListQuery request, CancellationToken cancellationToken)
+	{
+		Result<BlogListModelV1> result;
+
+		if(request.PageNo <= 0)
+		{
+			result = Result<BlogListModelV1>.Fail(MessageResource.InvalidPageNo);
+			goto result;
+		}
+
+		if (request.PageSize <= 0)
+		{
+			result = Result<BlogListModelV1>.Fail(MessageResource.InvalidPageSize);
+			goto result;
+		}
+
+		result = await _blogRepository.GetBlogsAsync(request.PageNo, request.PageSize, cancellationToken);
+
+		result:
+		return result;
+	}
 }
+
