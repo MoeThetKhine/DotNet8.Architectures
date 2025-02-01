a7823f3a3300ffed18fdf755ea769b9b9f8514ac
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-01 22:05:12 +0630

    Format Code

--- DotNet8.Architectures.Clean.Application/Features/Blog/GetBlogById/GetBlogByIdQueryHandler.cs
+++ DotNet8.Architectures.Clean.Application/Features/Blog/GetBlogById/GetBlogByIdQueryHandler.cs
@@ -1,28 +1,27 @@
-﻿namespace DotNet8.Architectures.Clean.Application.Features.Blog.GetBlogById
+﻿namespace DotNet8.Architectures.Clean.Application.Features.Blog.GetBlogById;
+
+public class GetBlogByIdQueryHandler : IRequestHandler<GetBlogByIdQuery, Result<BlogModel>>
 {
-	public class GetBlogByIdQueryHandler : IRequestHandler<GetBlogByIdQuery, Result<BlogModel>>
+	private readonly IBlogRepository _blogRepository;
+
+	public GetBlogByIdQueryHandler(IBlogRepository blogRepository)
 	{
-		private readonly IBlogRepository _blogRepository;
+		_blogRepository = blogRepository;
+	}
 
-		public GetBlogByIdQueryHandler(IBlogRepository blogRepository)
+	public async Task<Result<BlogModel>> Handle (GetBlogByIdQuery request, CancellationToken cancellationToken)
+	{
+		Result<BlogModel> result;
+
+		if (request.BlogId <= 0)
 		{
-			_blogRepository = blogRepository;
+			result = Result<BlogModel>.Fail(MessageResource.InvalidId);
+			goto result;
 		}
 
-		public async Task<Result<BlogModel>> Handle (GetBlogByIdQuery request, CancellationToken cancellationToken)
-		{
-			Result<BlogModel> result;
+		result = await _blogRepository.GetBlogByIdAsync(request.BlogId, cancellationToken);
 
-			if (request.BlogId <= 0)
-			{
-				result = Result<BlogModel>.Fail(MessageResource.InvalidId);
-				goto result;
-			}
-
-			result = await _blogRepository.GetBlogByIdAsync(request.BlogId, cancellationToken);
-
-		result:
-			return result;
-		}
+	result:
+		return result;
 	}
 }
