c6dbe7f66300966269392a070210d55b53c893c1
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-01 17:10:10 +0630

    -

--- DotNet8.Architectures.Clean.Infrastructure/Features/Blog/BlogDbContext.cs
+++ DotNet8.Architectures.Clean.Infrastructure/Features/Blog/BlogDbContext.cs
@@ -1,8 +1,12 @@
 ﻿namespace DotNet8.Architectures.Clean.Infrastructure.Features.Blog;
 
+#region BlogDbContext
+
 public class BlogDbContext : DbContext
 {
 	public BlogDbContext(DbContextOptions options) : base(options) { }
 
 	public DbSet<Tbl_Blog> Tbl_Blogs { get; set; }
 }
+
+#endregion
