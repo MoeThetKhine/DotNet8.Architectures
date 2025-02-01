771833b013175c14db879a9f5f066478196287a0
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-01 17:08:37 +0630

    -

--- DotNet8.Architectures.Clean.Domain/Features/Blog/IBlogRepository.cs
+++ DotNet8.Architectures.Clean.Domain/Features/Blog/IBlogRepository.cs
@@ -1,6 +1,10 @@
 ﻿namespace DotNet8.Architectures.Clean.Domain.Features.Blog;
 
+#region IBlogRepository
+
 public interface IBlogRepository
 {
 	Task<Result<BlogListModelV1>> GetBlogsAsync(int pageNo, int pageSize, CancellationToken cancellationToken);
 }
+
+#endregion
