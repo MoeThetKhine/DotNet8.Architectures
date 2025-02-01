c7f06f4eca22cf7e2f0b685f08ed04f76cfcfca3
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-01 22:06:34 +0630

    -

--- DotNet8.Architectures.Clean.Application/Features/Blog/GetBlogById/GetBlogByIdQuery.cs
+++ DotNet8.Architectures.Clean.Application/Features/Blog/GetBlogById/GetBlogByIdQuery.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DotNet8.Architectures.Clean.Application.Features.Blog.GetBlogById
+﻿namespace DotNet8.Architectures.Clean.Application.Features.Blog.GetBlogById
 {
 	public class GetBlogByIdQuery : IRequest<Result<BlogModel>>
 	{
