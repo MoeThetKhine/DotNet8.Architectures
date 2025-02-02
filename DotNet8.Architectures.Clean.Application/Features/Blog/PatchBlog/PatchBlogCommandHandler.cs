07422aeacd09c56fee9ac85f3cf685e7bef65785
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-02 22:03:48 +0630

    Format Code

--- DotNet8.Architectures.Clean.Application/Features/Blog/PatchBlog/PatchBlogCommandHandler.cs
+++ DotNet8.Architectures.Clean.Application/Features/Blog/PatchBlog/PatchBlogCommandHandler.cs
@@ -4,35 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DotNet8.Architectures.Clean.Application.Features.Blog.PatchBlog
+namespace DotNet8.Architectures.Clean.Application.Features.Blog.PatchBlog;
+
+public class PatchBlogCommandHandler : IRequestHandler<PatchBlogCommand , Result<BlogModel>>
 {
-	public class PatchBlogCommandHandler : IRequestHandler<PatchBlogCommand , Result<BlogModel>>
+	private readonly IBlogRepository _blogRepository;
+
+	public PatchBlogCommandHandler(IBlogRepository blogRepository)
 	{
-		private readonly IBlogRepository _blogRepository;
-
-		public PatchBlogCommandHandler(IBlogRepository blogRepository)
-		{
-			_blogRepository = blogRepository;
-		}
-
-		#region Handle
-
-		public async Task<Result<BlogModel>> Handle(PatchBlogCommand request, CancellationToken cancellationToken)
-		{
-			Result<BlogModel> result;
-
-			if(request.BlogId <= 0)
-			{
-				result = Result<BlogModel>.Fail(MessageResource.InvalidId);
-				goto result;
-			}
-
-			result = await _blogRepository.PatchBlogAsync(request.BlogRequestModel, request.BlogId, cancellationToken);
-
-		result:
-			return result;
-		}
-
-		#endregion
+		_blogRepository = blogRepository;
 	}
+
+	#region Handle
+
+	public async Task<Result<BlogModel>> Handle(PatchBlogCommand request, CancellationToken cancellationToken)
+	{
+		Result<BlogModel> result;
+
+		if(request.BlogId <= 0)
+		{
+			result = Result<BlogModel>.Fail(MessageResource.InvalidId);
+			goto result;
+		}
+
+		result = await _blogRepository.PatchBlogAsync(request.BlogRequestModel, request.BlogId, cancellationToken);
+
+	result:
+		return result;
+	}
+
+	#endregion
 }
