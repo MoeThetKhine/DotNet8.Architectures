3487d3c6fc5fa38fa84b9d0342a2382de80d42f1
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-02 22:04:26 +0630

    Format Code

--- DotNet8.Architectures.Clean.Application/Features/Blog/PatchBlog/PatchBlogCommand.cs
+++ DotNet8.Architectures.Clean.Application/Features/Blog/PatchBlog/PatchBlogCommand.cs
@@ -1,15 +1,14 @@
-﻿namespace DotNet8.Architectures.Clean.Application.Features.Blog.PatchBlog
+﻿namespace DotNet8.Architectures.Clean.Application.Features.Blog.PatchBlog;
+
+public class PatchBlogCommand : IRequest<Result<BlogModel>>
 {
-	public class PatchBlogCommand : IRequest<Result<BlogModel>>
+	public BlogRequestModel BlogRequestModel { get; set; }
+
+	public int BlogId { get; set; }
+
+	public PatchBlogCommand(BlogRequestModel blogRequestModel, int blogId)
 	{
-		public BlogRequestModel BlogRequestModel { get; set; }
-
-		public int BlogId { get; set; }
-
-		public PatchBlogCommand(BlogRequestModel blogRequestModel, int blogId)
-		{
-			BlogRequestModel = blogRequestModel;
-			BlogId = blogId;
-		}
+		BlogRequestModel = blogRequestModel;
+		BlogId = blogId;
 	}
 }
