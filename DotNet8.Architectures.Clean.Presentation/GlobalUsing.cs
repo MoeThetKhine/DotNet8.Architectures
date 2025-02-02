9774cf2322168530c60690c80a7e9075f0de54b1
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-02 21:23:48 +0630

    Feat : Add Global Using

--- DotNet8.Architectures.Clean.Presentation/GlobalUsing.cs
+++ DotNet8.Architectures.Clean.Presentation/GlobalUsing.cs
@@ -10,5 +10,4 @@ global using DotNet8.Architectures.Shared;
 global using DotNet8.Architectures.Clean.Application.Features.Blog.GetBlogById;
 global using DotNet8.Architectures.Clean.Application.Features.Blog.CreateBlog;
 global using DotNet8.Architectures.DTO.Features.Blog;
-
-
+global using DotNet8.Architectures.Clean.Application.Features.Blog.UpdateBlog;
