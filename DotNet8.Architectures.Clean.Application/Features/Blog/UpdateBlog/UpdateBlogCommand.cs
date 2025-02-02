8a703bd7db85b431ad7cbf5c483d4eae0612ceef
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-02 21:21:42 +0630

    -

--- DotNet8.Architectures.Clean.Application/Features/Blog/UpdateBlog/UpdateBlogCommand.cs
+++ DotNet8.Architectures.Clean.Application/Features/Blog/UpdateBlog/UpdateBlogCommand.cs
@@ -5,9 +5,14 @@ public class UpdateBlogCommand : IRequest<Result<BlogModel>>
 	public BlogRequestModel RequestModel { get; set; }
 	public int BlogId {  get; set; }
 
+	#region UpdateBlogCommand
+
 	public UpdateBlogCommand(BlogRequestModel requestModel, int blogId)
 	{
 		RequestModel = requestModel;
 		BlogId = blogId;
 	}
+
+	#endregion
+
 }
