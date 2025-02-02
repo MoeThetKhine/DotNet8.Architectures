c47f875705990cbc51da61e8997661e9942ef67d
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-02 23:09:08 +0630

    -

--- DotNet8.Architectures.Clean.Presentation/Controllers/Blog/BlogController.cs
+++ DotNet8.Architectures.Clean.Presentation/Controllers/Blog/BlogController.cs
@@ -78,6 +78,8 @@ public class BlogController : BaseController
 
 	#endregion
 
+	#region DeleteBlog
+
 	[HttpDelete("{id}")]
 	public async Task<IActionResult> DeleteBlog(int id, CancellationToken cancellationToken)
 	{
@@ -85,4 +87,7 @@ public class BlogController : BaseController
 		var result = await _mediator.Send(command, cancellationToken);
 		return Content(result);
 	}
+
+	#endregion
+
 }
