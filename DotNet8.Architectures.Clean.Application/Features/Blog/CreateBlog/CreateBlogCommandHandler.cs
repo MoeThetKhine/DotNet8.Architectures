bf5367fa20355b67a20bfa9f158e0df40251c3ac
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-02 19:52:20 +0630

    Feat : Add CreateBlogCommandHandler

--- DotNet8.Architectures.Clean.Application/Features/Blog/CreateBlog/CreateBlogCommandHandler.cs
+++ DotNet8.Architectures.Clean.Application/Features/Blog/CreateBlog/CreateBlogCommandHandler.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DotNet8.Architectures.Shared;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,47 @@ using System.Threading.Tasks;
 
 namespace DotNet8.Architectures.Clean.Application.Features.Blog.CreateBlog
 {
-	internal class CreateBlogCommandHandler
+	public class CreateBlogCommandHandler : IRequestHandler<CreateBlogCommand, Result<BlogModel>>
 	{
+		private readonly IBlogRepository _blogRepository;
+
+		public CreateBlogCommandHandler(IBlogRepository blogRepository)
+		{
+			_blogRepository = blogRepository;
+		}
+
+		public async Task<Result<BlogModel>> Handle(CreateBlogCommand request, CancellationToken cancellationToken)
+		{
+			Result<BlogModel> result;
+
+			try
+			{
+				if (request.requestModel.BlogTitle.IsNullOrEmpty())
+				{
+					result = Result<BlogModel>.Fail("Blog Title cannot be empty.");
+					goto result;
+				}
+
+				if (request.requestModel.BlogAuthor.IsNullOrEmpty())
+				{
+					result = Result<BlogModel>.Fail("Blog Author cannot be empty.");
+					goto result;
+				}
+
+				if (request.requestModel.BlogContent.IsNullOrEmpty())
+				{
+					result = Result<BlogModel>.Fail("Blog Content cannot be empty.");
+					goto result;
+				}
+
+				result = await _blogRepository.CreateBlogAsync(request.requestModel, cancellationToken);
+			}
+			catch (Exception ex)
+			{
+				result = Result<BlogModel>.Failure(ex);
+			}
+			result:
+			return result;
+		}
 	}
 }
