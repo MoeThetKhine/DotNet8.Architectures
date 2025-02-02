df501027d088d6200dcf51b0841c5a8dbe1f2998
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-02 19:53:24 +0630

    Feat : Add CreateBlogAsync in BlogRepository

--- DotNet8.Architectures.Clean.Infrastructure/Features/Blog/BlogRepository.cs
+++ DotNet8.Architectures.Clean.Infrastructure/Features/Blog/BlogRepository.cs
@@ -91,4 +91,30 @@ public class BlogRepository : IBlogRepository
 
 	#endregion
 
+	public async Task<Result<BlogModel>> CreateBlogAsync(BlogRequestModel blogRequest, CancellationToken cancellationToken)
+	{
+		Result<BlogModel> result;
+
+		try
+		{
+			var blog = new Tbl_Blog()
+			{
+				BlogTitle = blogRequest.BlogTitle,
+				BlogAuthor = blogRequest.BlogAuthor,
+				BlogContent = blogRequest.BlogContent,
+			};
+
+			await _context.Tbl_Blogs.AddAsync(blog, cancellationToken);
+			await _context.SaveChangesAsync();
+
+			result = Result<BlogModel>.SaveSuccess();
+		}
+		catch(Exception ex)
+		{
+			result = Result<BlogModel>.Failure(ex);
+		}
+	result:
+		return result;
+	}
+
 }
