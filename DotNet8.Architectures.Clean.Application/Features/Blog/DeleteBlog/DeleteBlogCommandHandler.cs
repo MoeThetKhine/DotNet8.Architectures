8545201c5d150ae95fad46cd4f35567652344cd9
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-02 23:07:42 +0630

    Format Code

--- DotNet8.Architectures.Clean.Application/Features/Blog/DeleteBlog/DeleteBlogCommandHandler.cs
+++ DotNet8.Architectures.Clean.Application/Features/Blog/DeleteBlog/DeleteBlogCommandHandler.cs
@@ -1,31 +1,30 @@
-﻿namespace DotNet8.Architectures.Clean.Application.Features.Blog.DeleteBlog
+﻿namespace DotNet8.Architectures.Clean.Application.Features.Blog.DeleteBlog;
+
+public class DeleteBlogCommandHandler : IRequestHandler<DeleteBlogCommand, Result<BlogModel>>
 {
-	public class DeleteBlogCommandHandler : IRequestHandler<DeleteBlogCommand, Result<BlogModel>
+	private readonly IBlogRepository _blogRepository;
+
+	public DeleteBlogCommandHandler(IBlogRepository blogRepository)
 	{
-		private readonly IBlogRepository _blogRepository;
-
-		public DeleteBlogCommandHandler(IBlogRepository blogRepository)
-		{
-			_blogRepository = blogRepository;
-		}
-
-		#region Handle
-
-		public async Task<Result<BlogModel>> Handle(DeleteBlogCommand request , CancellationToken cancellationToken)
-		{
-			Result<BlogModel> result;
-
-			if(request.BlogId <= 0)
-			{
-				result = Result<BlogModel>.Fail(MessageResource.InvalidId);
-				goto result;
-			}
-			result = await _blogRepository.DelteBlogAsync(request.BlogId, cancellationToken);
-
-			result:
-			return result;
-		}
-
-		#endregion
+		_blogRepository = blogRepository;
 	}
+
+	#region Handle
+
+	public async Task<Result<BlogModel>> Handle(DeleteBlogCommand request , CancellationToken cancellationToken)
+	{
+		Result<BlogModel> result;
+
+		if(request.BlogId <= 0)
+		{
+			result = Result<BlogModel>.Fail(MessageResource.InvalidId);
+			goto result;
+		}
+		result = await _blogRepository.DelteBlogAsync(request.BlogId, cancellationToken);
+
+		result:
+		return result;
+	}
+
+	#endregion
 }
