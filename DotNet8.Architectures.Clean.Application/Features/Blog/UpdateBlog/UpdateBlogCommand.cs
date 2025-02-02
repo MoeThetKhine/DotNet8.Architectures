707a27eeb933062c014016d3100f8c60e9b71573
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-02 21:11:47 +0630

    Feat : Add UpdateBlogCommand

--- DotNet8.Architectures.Clean.Application/Features/Blog/UpdateBlog/UpdateBlogCommand.cs
+++ DotNet8.Architectures.Clean.Application/Features/Blog/UpdateBlog/UpdateBlogCommand.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace DotNet8.Architectures.Clean.Application.Features.Blog.UpdateBlog
 {
-	internal class UpdateBlogCommand
+	public class UpdateBlogCommand : IRequest<Result<BlogModel>>
 	{
+		public BlogRequestModel RequestModel { get; set; }
+		public int BlogId {  get; set; }
+
+		public UpdateBlogCommand(BlogRequestModel requestModel, int blogId)
+		{
+			RequestModel = requestModel;
+			BlogId = blogId;
+		}
 	}
 }
