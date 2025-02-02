02a4c936a5422b8144961e484b23b2ba6eda9ff1
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-02 19:59:15 +0630

    -

--- DotNet8.Architectures.Clean.Application/Features/Blog/CreateBlog/CreateBlogCommand.cs
+++ DotNet8.Architectures.Clean.Application/Features/Blog/CreateBlog/CreateBlogCommand.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace DotNet8.Architectures.Clean.Application.Features.Blog.CreateBlog
 {
 	public class CreateBlogCommand : IRequest<Result<BlogModel>>
