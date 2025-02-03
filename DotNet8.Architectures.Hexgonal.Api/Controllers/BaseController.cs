f11585dbd30ede6df773178a044c66667b5b50f4
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-03 19:25:06 +0630

    -

--- DotNet8.Architectures.Hexgonal.Api/Controllers/BaseController.cs
+++ DotNet8.Architectures.Hexgonal.Api/Controllers/BaseController.cs
@@ -1,15 +1,14 @@
 ﻿using DotNet8.Architectures.Shared;
 using Microsoft.AspNetCore.Mvc;
 
-namespace DotNet8.Architectures.Hexgonal.Api.Controllers
+namespace DotNet8.Architectures.Hexgonal.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BaseController : ControllerBase
 {
-	[Route("api/[controller]")]
-	[ApiController]
-	public class BaseController : ControllerBase
+	public IActionResult Content(Object obj)
 	{
-		public IActionResult Content(Object obj)
-		{
-			return Content(obj.ToJson(), "application/json");
-		}
+		return Content(obj.ToJson(), "application/json");
 	}
 }
