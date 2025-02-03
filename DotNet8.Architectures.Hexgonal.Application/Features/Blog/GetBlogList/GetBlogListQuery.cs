48ee5e76bb21ab169a74f8a0fdcdfc6d463428f0
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-03 21:31:49 +0630

    Format Code

--- DotNet8.Architectures.Hexgonal.Application/Features/Blog/GetBlogList/GetBlogListQuery.cs
+++ DotNet8.Architectures.Hexgonal.Application/Features/Blog/GetBlogList/GetBlogListQuery.cs
@@ -1,14 +1,13 @@
-﻿namespace DotNet8.Architectures.Hexgonal.Application.Features.Blog.GetBlogList
-{
-	public class GetBlogListQuery : IRequest<Result<BlogListModelV1>>
-	{
-		public int PageNo {  get; set; }
-		public int PageSize { get; set; }
+﻿namespace DotNet8.Architectures.Hexgonal.Application.Features.Blog.GetBlogList;
 
-		public GetBlogListQuery(int pageNo, int pageSize)
-		{
-			PageNo = pageNo;
-			PageSize = pageSize;
-		}
+public class GetBlogListQuery : IRequest<Result<BlogListModelV1>>
+{
+	public int PageNo {  get; set; }
+	public int PageSize { get; set; }
+
+	public GetBlogListQuery(int pageNo, int pageSize)
+	{
+		PageNo = pageNo;
+		PageSize = pageSize;
 	}
 }
