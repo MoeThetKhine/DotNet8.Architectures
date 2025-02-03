21bca6b7d24c2f8f56060cc141cd4c833dc73406
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-03 19:58:16 +0630

    -

--- DotNet8.Architectures.Hexgonal.Application/Features/Blog/BlogValidator.cs
+++ DotNet8.Architectures.Hexgonal.Application/Features/Blog/BlogValidator.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DotNet8.Architectures.Hexgonal.Application.Features.Blog
+﻿namespace DotNet8.Architectures.Hexgonal.Application.Features.Blog
 {
-	internal class BlogValidator
+	pub class BlogValidator
 	{
 	}
 }
