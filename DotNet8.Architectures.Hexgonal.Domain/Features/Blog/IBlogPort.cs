2c86bc77dffa751f4d047a1fbf829b7a01407291
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-03 19:22:01 +0630

    -

--- DotNet8.Architectures.Hexgonal.Domain/Features/Blog/IBlogPort.cs
+++ DotNet8.Architectures.Hexgonal.Domain/Features/Blog/IBlogPort.cs
@@ -3,7 +3,11 @@ using DotNet8.Architectures.Utils;
 
 namespace DotNet8.Architectures.Hexgonal.Domain.Features.Blog;
 
+#region IBlogPort
+
 public interface IBlogPort
 {
 	Task<Result<BlogListModelV1>> GetBlogAsync(int pageNo, int pageSize, CancellationToken cancellationToken);
 }
+
+#endregion
