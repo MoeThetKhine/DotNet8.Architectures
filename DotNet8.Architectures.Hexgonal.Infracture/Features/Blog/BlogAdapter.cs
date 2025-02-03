4bfe0bffada961975d19994044fe5d5cc730906f
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-03 19:12:57 +0630

    Feat : Add GetBlogAsync in BlogAdapter

--- DotNet8.Architectures.Hexgonal.Infracture/Features/Blog/BlogAdapter.cs
+++ DotNet8.Architectures.Hexgonal.Infracture/Features/Blog/BlogAdapter.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿using DotNet8.Architectures.DbServices.Models;
+using DotNet8.Architectures.DTO.Features.Blog;
+using DotNet8.Architectures.DTO.PageSetting;
+using DotNet8.Architectures.Hexgonal.Domain.Features.Blog;
+using DotNet8.Architectures.Shared;
+using DotNet8.Architectures.Utils;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +13,53 @@ using System.Threading.Tasks;
 
 namespace DotNet8.Architectures.Hexgonal.Infracture.Features.Blog
 {
-	internal class BlogAdapter
+	public class BlogAdapter : IBlogPort
 	{
+		private readonly AppDbContext _context;
+
+		public BlogAdapter(AppDbContext context)
+		{
+			_context = context;
+		}
+
+		public async Task<Result<BlogListModelV1>> GetBlogAsync(int pageNo, int pageSize, CancellationToken cancellationToken)
+		{
+			Result<BlogListModelV1> result;
+
+			try
+			{
+				var query = _context.Tbl_Blogs.OrderByDescending(x => x.BlogId);
+				var lst = await query.Paginate(pageNo,pageSize)
+					.ToListAsync(cancellationToken: cancellationToken);
+				var totalCount = await query.CountAsync(cancellationToken);
+				var pageCount = totalCount / pageSize;
+
+				if(totalCount % pageSize > 0)
+				{
+					pageCount++;
+				}
+
+				var pageSettingModel = new PageSettingModel(pageNo, pageSize, pageCount, totalCount);
+				var model = new BlogListModelV1()
+				{
+					DataLst = lst.Select(x => new BlogModel()
+					{
+						BlogId = x.BlogId,
+						BlogTitle = x.BlogTitle,
+						BlogAuthor = x.BlogAuthor,
+						BlogContent = x.BlogContent,
+					}).AsQueryable(),
+					PageSetting = pageSettingModel
+				};
+
+				result = Result<BlogListModelV1>.Success(model);
+			}
+			catch (Exception ex)
+			{
+				result = Result<BlogListModelV1>.Failure(ex);
+			}
+
+			return result;
+		}
 	}
 }
