4b10ab44e44faff9400d5364606fc62735f61f51
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-03 23:23:02 +0630

    Format Code

--- DotNet8.Architectures.Hexgonal.Api/Extensions/DependencyInjection.cs
+++ DotNet8.Architectures.Hexgonal.Api/Extensions/DependencyInjection.cs
@@ -4,47 +4,46 @@ using DotNet8.Architectures.Hexgonal.Infracture.Features.Blog;
 using Microsoft.EntityFrameworkCore;
 using System.Runtime.CompilerServices;
 
-namespace DotNet8.Architectures.Hexgonal.Api.Extensions
+namespace DotNet8.Architectures.Hexgonal.Api.Extensions;
+
+public static class DependencyInjection
 {
-	public static class DependencyInjection
+	#region AddDbContextService
+
+	private static IServiceCollection AddDbContextService(this IServiceCollection services, WebApplicationBuilder builder)
 	{
-		#region AddDbContextService
+		builder.Services.AddDbContext<AppDbContext>(
+			opt =>
+			{
+				opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnetion"));
+				opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+			},
+			ServiceLifetime.Transient,
+			ServiceLifetime.Transient
 
-		private static IServiceCollection AddDbContextService(this IServiceCollection services, WebApplicationBuilder builder)
-		{
-			builder.Services.AddDbContext<AppDbContext>(
-				opt =>
-				{
-					opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnetion"));
-					opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
-				},
-				ServiceLifetime.Transient,
-				ServiceLifetime.Transient
+		);
 
-			);
-
-			return services;
-		}
-
-		#endregion
-
-		#region AddRepositoryService
-
-		private static IServiceCollection AddRepositoryService(this IServiceCollection services)
-		{
-			return services.AddScoped<IBlogPort, BlogAdapter>();
-		}
-
-		#endregion
-
-		#region AddDependencyInjection
-
-		public static IServiceCollection AddDependencyInjection(this IServiceCollection services, WebApplicationBuilder builder)
-		{
-			return services.AddDbContextService(builder).AddRepositoryService();
-		}
-
-		#endregion
-
+		return services;
 	}
+
+	#endregion
+
+	#region AddRepositoryService
+
+	private static IServiceCollection AddRepositoryService(this IServiceCollection services)
+	{
+		return services.AddScoped<IBlogPort, BlogAdapter>();
+	}
+
+	#endregion
+
+	#region AddDependencyInjection
+
+	public static IServiceCollection AddDependencyInjection(this IServiceCollection services, WebApplicationBuilder builder)
+	{
+		return services.AddDbContextService(builder).AddRepositoryService();
+	}
+
+	#endregion
+
 }
