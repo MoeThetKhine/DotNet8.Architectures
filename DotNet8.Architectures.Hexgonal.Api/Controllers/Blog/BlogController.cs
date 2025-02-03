e9211df469788dcb49cf8fb0c4549389b2a71663
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-03 21:28:31 +0630

    Feat : Add Global Using

--- DotNet8.Architectures.Hexgonal.Api/Controllers/Blog/BlogController.cs
+++ DotNet8.Architectures.Hexgonal.Api/Controllers/Blog/BlogController.cs
@@ -1,7 +1,4 @@
-﻿using DotNet8.Architectures.Hexgonal.Application.Features.Blog.GetBlogList;
-using MediatR;
-
-namespace DotNet8.Architectures.Hexgonal.Api.Controllers.Blog
+﻿namespace DotNet8.Architectures.Hexgonal.Api.Controllers.Blog
 {
 	[Route("api/[controller]")]
 	[ApiController]
--- DotNet8.Architectures.Hexgonal.Api/GlobalUsing.cs
+++ DotNet8.Architectures.Hexgonal.Api/GlobalUsing.cs
@@ -1,2 +1,4 @@
 ﻿global using DotNet8.Architectures.Shared;
 global using Microsoft.AspNetCore.Mvc;
+global using DotNet8.Architectures.Hexgonal.Application.Features.Blog.GetBlogList;
+global using MediatR;
