681240f588357b2134562268a48ecd0ec949c056
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-03 17:41:12 +0630

    Feat : Add GetBlogAsync in IBlogPort

--- DotNet8.Architectures.Hexgonal.Domain/Features/Blog/IBlogPort.cs
+++ DotNet8.Architectures.Hexgonal.Domain/Features/Blog/IBlogPort.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DotNet8.Architectures.DTO.Features.Blog;
+using DotNet8.Architectures.Utils;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,9 @@ using System.Threading.Tasks;
 
 namespace DotNet8.Architectures.Hexgonal.Domain.Features.Blog
 {
-	internal class IBlogPort
+	public interface IBlogPort
 	{
+		Task<Result<BlogListModelV1>> GetBlogAsync(int pageNo, int pageSize, CancellationToken cancellationToken);
 	}
 }
+	
