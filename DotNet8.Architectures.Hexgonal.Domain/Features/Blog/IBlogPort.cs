4aab2fbe3b161ed1de45be336998b2e8cd21ee07
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-03 19:21:41 +0630

    Format Code

--- DotNet8.Architectures.Hexgonal.Domain/Features/Blog/IBlogPort.cs
+++ DotNet8.Architectures.Hexgonal.Domain/Features/Blog/IBlogPort.cs
@@ -1,11 +1,9 @@
 ﻿using DotNet8.Architectures.DTO.Features.Blog;
 using DotNet8.Architectures.Utils;
 
-namespace DotNet8.Architectures.Hexgonal.Domain.Features.Blog
+namespace DotNet8.Architectures.Hexgonal.Domain.Features.Blog;
+
+public interface IBlogPort
 {
-	public interface IBlogPort
-	{
-		Task<Result<BlogListModelV1>> GetBlogAsync(int pageNo, int pageSize, CancellationToken cancellationToken);
-	}
+	Task<Result<BlogListModelV1>> GetBlogAsync(int pageNo, int pageSize, CancellationToken cancellationToken);
 }
-	
