2f9982cd1c4ece6ea944aab118d0d95fcf8c656f
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-04 21:34:26 +0630

    Feat : Add GetBlogIdQueryHandler

--- DotNet8.Architectures.Hexgonal.Application/Features/Blog/GetBlogById/GetBlogIdQueryHandler.cs
+++ DotNet8.Architectures.Hexgonal.Application/Features/Blog/GetBlogById/GetBlogIdQueryHandler.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace DotNet8.Architectures.Hexgonal.Application.Features.Blog.GetBlogById
 {
-	internal class GetBlogIdQueryHandler
+	public class GetBlogIdQueryHandler : IRequestHandler<GetBlogIdQuery , Result<BlogModel>>
 	{
+		private readonly IBlogPort _blogPort;
+
+		public GetBlogIdQueryHandler(IBlogPort blogPort)
+		{
+			_blogPort = blogPort;
+		}
+
+		public async Task<Result<BlogModel>> Handle(GetBlogIdQuery request, CancellationToken cancellationToken)
+		{
+			Result<BlogModel> result;
+
+			if (request.BlogId <= 0)
+			{
+				result = Result<BlogModel>.Fail(MessageResource.InvalidId);
+				goto result;
+			}
+
+			result = await _blogPort.GetBlogByIdAsync(request.BlogId, cancellationToken);
+				
+			result:
+			return result;
+		}
 	}
 }
