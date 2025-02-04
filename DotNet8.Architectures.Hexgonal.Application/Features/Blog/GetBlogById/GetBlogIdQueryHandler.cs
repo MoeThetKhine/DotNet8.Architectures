e8888c6655d05603969a4703709abb16e2db90d2
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-04 21:35:15 +0630

    Format Code

--- DotNet8.Architectures.Hexgonal.Application/Features/Blog/GetBlogById/GetBlogIdQueryHandler.cs
+++ DotNet8.Architectures.Hexgonal.Application/Features/Blog/GetBlogById/GetBlogIdQueryHandler.cs
@@ -1,28 +1,27 @@
-﻿namespace DotNet8.Architectures.Hexgonal.Application.Features.Blog.GetBlogById
+﻿namespace DotNet8.Architectures.Hexgonal.Application.Features.Blog.GetBlogById;
+
+public class GetBlogIdQueryHandler : IRequestHandler<GetBlogIdQuery , Result<BlogModel>>
 {
-	public class GetBlogIdQueryHandler : IRequestHandler<GetBlogIdQuery , Result<BlogModel>>
+	private readonly IBlogPort _blogPort;
+
+	public GetBlogIdQueryHandler(IBlogPort blogPort)
 	{
-		private readonly IBlogPort _blogPort;
+		_blogPort = blogPort;
+	}
 
-		public GetBlogIdQueryHandler(IBlogPort blogPort)
+	public async Task<Result<BlogModel>> Handle(GetBlogIdQuery request, CancellationToken cancellationToken)
+	{
+		Result<BlogModel> result;
+
+		if (request.BlogId <= 0)
 		{
-			_blogPort = blogPort;
+			result = Result<BlogModel>.Fail(MessageResource.InvalidId);
+			goto result;
 		}
 
-		public async Task<Result<BlogModel>> Handle(GetBlogIdQuery request, CancellationToken cancellationToken)
-		{
-			Result<BlogModel> result;
-
-			if (request.BlogId <= 0)
-			{
-				result = Result<BlogModel>.Fail(MessageResource.InvalidId);
-				goto result;
-			}
-
-			result = await _blogPort.GetBlogByIdAsync(request.BlogId, cancellationToken);
-				
-			result:
-			return result;
-		}
+		result = await _blogPort.GetBlogByIdAsync(request.BlogId, cancellationToken);
+			
+		result:
+		return result;
 	}
 }
