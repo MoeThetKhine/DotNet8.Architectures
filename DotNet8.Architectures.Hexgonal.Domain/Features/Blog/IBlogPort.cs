7bef9d04d9ecc4fa6c1dc5f440269d903f007217
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-04 21:33:51 +0630

    Feat: Add GetBlogByIdAsync in IBlogPort

--- DotNet8.Architectures.Hexgonal.Domain/Features/Blog/IBlogPort.cs
+++ DotNet8.Architectures.Hexgonal.Domain/Features/Blog/IBlogPort.cs
@@ -5,6 +5,8 @@
 public interface IBlogPort
 {
 	Task<Result<BlogListModelV1>> GetBlogAsync(int pageNo, int pageSize, CancellationToken cancellationToken);
+
+	Task<Result<BlogModel>> GetBlogByIdAsync(int id, CancellationToken cancellationToken);
 }
 
 #endregion
