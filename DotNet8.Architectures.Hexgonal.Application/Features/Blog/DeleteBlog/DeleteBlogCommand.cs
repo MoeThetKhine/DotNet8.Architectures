30e7b6e82986dd7fc6b8f89108950612bad9bb6e
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-05 19:54:21 +0630

    -

--- DotNet8.Architectures.Hexgonal.Application/Features/Blog/DeleteBlog/DeleteBlogCommand.cs
+++ DotNet8.Architectures.Hexgonal.Application/Features/Blog/DeleteBlog/DeleteBlogCommand.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DotNet8.Architectures.Hexgonal.Application.Features.Blog.DeleteBlog
+﻿namespace DotNet8.Architectures.Hexgonal.Application.Features.Blog.DeleteBlog
 {
 	public class DeleteBlogCommand : IRequest<Result<BlogModel>>
 	{
