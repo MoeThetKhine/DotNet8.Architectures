13a8f9d0b5396a505d7dba808baec7534b2f7a57
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-05 12:06:18 +0630

    Feat : Add CreateBlogCommand

--- DotNet8.Architectures.Hexgonal.Application/Features/Blog/CreateBlog/CreateBlogCommand.cs
+++ DotNet8.Architectures.Hexgonal.Application/Features/Blog/CreateBlog/CreateBlogCommand.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace DotNet8.Architectures.Hexgonal.Application.Features.Blog.CreateBlog
 {
-	internal class CreateBlogCommand
+	public class CreateBlogCommand : IRequest<Result<BlogModel>>
 	{
+		public BlogRequestModel requestModel;
+
+		public CreateBlogCommand(BlogRequestModel requestModel)
+		{
+			this.requestModel = requestModel;
+		}
 	}
 }
