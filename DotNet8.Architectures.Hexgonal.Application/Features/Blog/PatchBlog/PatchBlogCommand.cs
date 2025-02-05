96c9c0abaee93f61075aa84689c88ed605d9baa5
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-05 17:48:48 +0630

    Feat : Add PatchBlogCommand

--- DotNet8.Architectures.Hexgonal.Application/Features/Blog/PatchBlog/PatchBlogCommand.cs
+++ DotNet8.Architectures.Hexgonal.Application/Features/Blog/PatchBlog/PatchBlogCommand.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace DotNet8.Architectures.Hexgonal.Application.Features.Blog.PatchBlog
 {
-	internal class PatchBlogCommand
+	public class PatchBlogCommand : IRequest<Result<BlogModel>>
 	{
+		public BlogRequestModel BlogRequestModel { get; set; }
+
+		public int BlogId {  get; set; }
+
+		public PatchBlogCommand(BlogRequestModel blogRequestModel, int blogId )
+		{
+			BlogRequestModel = blogRequestModel;
+			BlogId = blogId;
+		}
 	}
+	
+	
 }
