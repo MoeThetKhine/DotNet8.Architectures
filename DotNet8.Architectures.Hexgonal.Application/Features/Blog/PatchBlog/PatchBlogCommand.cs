bf14b1b39c5dc6b32f6730d3f706f4fa67cf5c83
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-05 17:51:33 +0630

    -

--- DotNet8.Architectures.Hexgonal.Application/Features/Blog/PatchBlog/PatchBlogCommand.cs
+++ DotNet8.Architectures.Hexgonal.Application/Features/Blog/PatchBlog/PatchBlogCommand.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DotNet8.Architectures.Hexgonal.Application.Features.Blog.PatchBlog
+﻿namespace DotNet8.Architectures.Hexgonal.Application.Features.Blog.PatchBlog
 {
 	public class PatchBlogCommand : IRequest<Result<BlogModel>>
 	{
