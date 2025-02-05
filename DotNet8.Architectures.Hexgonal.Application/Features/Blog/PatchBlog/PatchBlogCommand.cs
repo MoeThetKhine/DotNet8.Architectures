c9809d99bdac2bd6b546583750ea09beb7fb84af
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-05 17:51:45 +0630

    Format Code

--- DotNet8.Architectures.Hexgonal.Application/Features/Blog/PatchBlog/PatchBlogCommand.cs
+++ DotNet8.Architectures.Hexgonal.Application/Features/Blog/PatchBlog/PatchBlogCommand.cs
@@ -1,17 +1,16 @@
-﻿namespace DotNet8.Architectures.Hexgonal.Application.Features.Blog.PatchBlog
+﻿namespace DotNet8.Architectures.Hexgonal.Application.Features.Blog.PatchBlog;
+
+public class PatchBlogCommand : IRequest<Result<BlogModel>>
 {
-	public class PatchBlogCommand : IRequest<Result<BlogModel>>
+	public BlogRequestModel BlogRequestModel { get; set; }
+
+	public int BlogId {  get; set; }
+
+	public PatchBlogCommand(BlogRequestModel blogRequestModel, int blogId )
 	{
-		public BlogRequestModel BlogRequestModel { get; set; }
-
-		public int BlogId {  get; set; }
-
-		public PatchBlogCommand(BlogRequestModel blogRequestModel, int blogId )
-		{
-			BlogRequestModel = blogRequestModel;
-			BlogId = blogId;
-		}
+		BlogRequestModel = blogRequestModel;
+		BlogId = blogId;
 	}
-	
-	
 }
+
+
