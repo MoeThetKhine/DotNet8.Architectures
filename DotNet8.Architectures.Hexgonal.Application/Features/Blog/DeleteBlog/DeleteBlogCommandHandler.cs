6e017d9765563d0f4ac40787b44468063d3eedb7
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-05 19:49:20 +0630

    Feat : Add DeleteBlogCommandHandler

--- DotNet8.Architectures.Hexgonal.Application/Features/Blog/DeleteBlog/DeleteBlogCommandHandler.cs
+++ DotNet8.Architectures.Hexgonal.Application/Features/Blog/DeleteBlog/DeleteBlogCommandHandler.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace DotNet8.Architectures.Hexgonal.Application.Features.Blog.DeleteBlog
 {
-	internal class DeleteBlogCommandHandler
+	public class DeleteBlogCommandHandler : IRequestHandler<DeleteBlogCommand, Result<BlogModel>>
 	{
+		private readonly IBlogPort _blogPort;
+
+		public DeleteBlogCommandHandler(IBlogPort blogPort)
+		{
+			_blogPort = blogPort;
+		}
+
+		public async Task<Result<BlogModel>> Handle(DeleteBlogCommand request, CancellationToken cancellationToken)
+		{
+			Result<BlogModel> result;
+
+			if(request.BlogId <= 0)
+			{
+				result = Result<BlogModel>.Fail(MessageResource.InvalidId);
+				goto result;
+			}
+
+			result = await _blogPort.DeleteBlogAsync(request.BlogId, cancellationToken);
+
+			result:
+			return result;
+
+		}
 	}
 }
