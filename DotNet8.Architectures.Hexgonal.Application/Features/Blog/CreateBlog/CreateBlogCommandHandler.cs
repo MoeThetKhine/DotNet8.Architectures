fdfc5a42c2817c290efd111a5238f7d3c18d73bc
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-05 12:06:29 +0630

    Feat : Add CreateBlogCommandHandler

--- DotNet8.Architectures.Hexgonal.Application/Features/Blog/CreateBlog/CreateBlogCommandHandler.cs
+++ DotNet8.Architectures.Hexgonal.Application/Features/Blog/CreateBlog/CreateBlogCommandHandler.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DotNet8.Architectures.Shared;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,41 @@ using System.Threading.Tasks;
 
 namespace DotNet8.Architectures.Hexgonal.Application.Features.Blog.CreateBlog
 {
-	internal class CreateBlogCommandHandler
+	public class CreateBlogCommandHandler : IRequestHandler<CreateBlogCommand , Result<BlogModel>>
 	{
+		private readonly IBlogPort _blogPort;
+
+		public CreateBlogCommandHandler(IBlogPort blogPort)
+		{
+			_blogPort = blogPort;
+		}
+
+		public async Task<Result<BlogModel>> Handle(CreateBlogCommand request, CancellationToken cancellationToken)
+		{
+			Result<BlogModel> result;
+
+			if(request.requestModel.BlogTitle.IsNullOrEmpty())
+			{
+				result = Result<BlogModel>.Fail("Blog Title cannot be empty.");
+				goto result;
+			}
+
+			if(request.requestModel.BlogAuthor.IsNullOrEmpty())
+			{
+				result = Result<BlogModel>.Fail("Blog Author cannot be empty.");
+				goto result;
+			}
+
+			if (request.requestModel.BlogAuthor.IsNullOrEmpty())
+			{
+				result = Result<BlogModel>.Fail("Blog Content cannot be empty.");
+				goto result;
+			}
+
+			result = await _blogPort.CreateBlogAsync(request.requestModel, cancellationToken);
+
+			result:
+			return result;
+		}
 	}
 }
