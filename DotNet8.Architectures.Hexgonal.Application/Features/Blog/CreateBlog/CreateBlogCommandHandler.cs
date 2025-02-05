9b4268bea7ab06b7c639cd644d763fd8b004108e
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-05 12:11:27 +0630

    Format Code

--- DotNet8.Architectures.Hexgonal.Application/Features/Blog/CreateBlog/CreateBlogCommandHandler.cs
+++ DotNet8.Architectures.Hexgonal.Application/Features/Blog/CreateBlog/CreateBlogCommandHandler.cs
@@ -1,46 +1,45 @@
 ﻿using DotNet8.Architectures.Shared;
 
-namespace DotNet8.Architectures.Hexgonal.Application.Features.Blog.CreateBlog
+namespace DotNet8.Architectures.Hexgonal.Application.Features.Blog.CreateBlog;
+
+public class CreateBlogCommandHandler : IRequestHandler<CreateBlogCommand , Result<BlogModel>>
 {
-	public class CreateBlogCommandHandler : IRequestHandler<CreateBlogCommand , Result<BlogModel>>
+	private readonly IBlogPort _blogPort;
+
+	public CreateBlogCommandHandler(IBlogPort blogPort)
 	{
-		private readonly IBlogPort _blogPort;
-
-		public CreateBlogCommandHandler(IBlogPort blogPort)
-		{
-			_blogPort = blogPort;
-		}
-
-		#region Handle
-
-		public async Task<Result<BlogModel>> Handle(CreateBlogCommand request, CancellationToken cancellationToken)
-		{
-			Result<BlogModel> result;
-
-			if(request.requestModel.BlogTitle.IsNullOrEmpty())
-			{
-				result = Result<BlogModel>.Fail("Blog Title cannot be empty.");
-				goto result;
-			}
-
-			if(request.requestModel.BlogAuthor.IsNullOrEmpty())
-			{
-				result = Result<BlogModel>.Fail("Blog Author cannot be empty.");
-				goto result;
-			}
-
-			if (request.requestModel.BlogAuthor.IsNullOrEmpty())
-			{
-				result = Result<BlogModel>.Fail("Blog Content cannot be empty.");
-				goto result;
-			}
-
-			result = await _blogPort.CreateBlogAsync(request.requestModel, cancellationToken);
-
-			result:
-			return result;
-		}
-
-		#endregion
+		_blogPort = blogPort;
 	}
+
+	#region Handle
+
+	public async Task<Result<BlogModel>> Handle(CreateBlogCommand request, CancellationToken cancellationToken)
+	{
+		Result<BlogModel> result;
+
+		if(request.requestModel.BlogTitle.IsNullOrEmpty())
+		{
+			result = Result<BlogModel>.Fail("Blog Title cannot be empty.");
+			goto result;
+		}
+
+		if(request.requestModel.BlogAuthor.IsNullOrEmpty())
+		{
+			result = Result<BlogModel>.Fail("Blog Author cannot be empty.");
+			goto result;
+		}
+
+		if (request.requestModel.BlogAuthor.IsNullOrEmpty())
+		{
+			result = Result<BlogModel>.Fail("Blog Content cannot be empty.");
+			goto result;
+		}
+
+		result = await _blogPort.CreateBlogAsync(request.requestModel, cancellationToken);
+
+		result:
+		return result;
+	}
+
+	#endregion
 }
