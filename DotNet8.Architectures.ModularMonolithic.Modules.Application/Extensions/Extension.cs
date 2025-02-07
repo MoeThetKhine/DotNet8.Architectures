cf5a6e82cb0a38f502b2775acfa02185b0d7f49a
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-07 21:17:39 +0630

    Format Code

--- DotNet8.Architectures.ModularMonolithic.Modules.Application/Extensions/Extension.cs
+++ DotNet8.Architectures.ModularMonolithic.Modules.Application/Extensions/Extension.cs
@@ -1,14 +1,12 @@
-﻿namespace DotNet8.Architectures.ModularMonolithic.Modules.Application.Extensions
-{
-	public static class Extension
-	{
+﻿namespace DotNet8.Architectures.ModularMonolithic.Modules.Application.Extensions;
 
-		public static IServiceCollection AddMediatRService(this IServiceCollection services)
-		{
-			return services.AddMediatR(cf =>
-		   cf.RegisterServicesFromAssembly(typeof(Extension).Assembly)
-	   );
-		}
-		
+public static class Extension
+{
+	public static IServiceCollection AddMediatRService(this IServiceCollection services)
+	{
+		return services.AddMediatR(cf =>
+	   cf.RegisterServicesFromAssembly(typeof(Extension).Assembly)
+   );
 	}
+	
 }
