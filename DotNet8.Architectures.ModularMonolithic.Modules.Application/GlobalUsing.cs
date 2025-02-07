95776781b88c89b2fd859f8e66fd043dbc4011af
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-07 21:13:45 +0630

    Feat : Add Global Using

--- DotNet8.Architectures.ModularMonolithic.Modules.Application/GlobalUsing.cs
+++ DotNet8.Architectures.ModularMonolithic.Modules.Application/GlobalUsing.cs
@@ -1,12 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DotNet8.Architectures.ModularMonolithic.Modules.Application
-{
-	internal class GlobalUsing
-	{
-	}
-}
+﻿global using Microsoft.Extensions.DependencyInjection;
