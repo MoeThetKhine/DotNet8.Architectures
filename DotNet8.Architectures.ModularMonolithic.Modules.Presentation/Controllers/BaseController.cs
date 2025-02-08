adc608232458023244de092c5a76775aaaafd5c1
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-08 21:13:38 +0630

    -

--- DotNet8.Architectures.ModularMonolithic.Modules.Presentation/Controllers/BaseController.cs
+++ DotNet8.Architectures.ModularMonolithic.Modules.Presentation/Controllers/BaseController.cs
@@ -4,8 +4,14 @@
 [ApiController]
 public class BaseController : ControllerBase
 {
+
+	#region Content
+
 	public IActionResult Content(object obj)
 	{
 		return Content(obj.ToJson(), "application/json");
 	}
+
+	#endregion
+
 }
