31d9712968494beb47658d5ca51ab8ad34ff60da
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-08 23:48:21 +0630

    Format Code

--- DotNet8.Architectures.ModularMonolithic.Modules.Application/Features/Blog/UpdateBlog/UpdateBlogCommand.cs
+++ DotNet8.Architectures.ModularMonolithic.Modules.Application/Features/Blog/UpdateBlog/UpdateBlogCommand.cs
@@ -1,6 +1,5 @@
-﻿namespace DotNet8.Architectures.ModularMonolithic.Modules.Application.Features.Blog.UpdateBlog
+﻿namespace DotNet8.Architectures.ModularMonolithic.Modules.Application.Features.Blog.UpdateBlog;
+
+internal class UpdateBlogCommand
 {
-	internal class UpdateBlogCommand
-	{
-	}
 }
