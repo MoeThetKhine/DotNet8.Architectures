8f39b4b84ba6155f9a1798ed768d26d5a0026368
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-08 21:13:09 +0630

    Format Code

--- DotNet8.Architectures.ModularMonolithic.Modules.Presentation/Controllers/BaseController.cs
+++ DotNet8.Architectures.ModularMonolithic.Modules.Presentation/Controllers/BaseController.cs
@@ -1,12 +1,11 @@
-﻿namespace DotNet8.Architectures.ModularMonolithic.Modules.Presentation.Controllers
+﻿namespace DotNet8.Architectures.ModularMonolithic.Modules.Presentation.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BaseController : ControllerBase
 {
-	[Route("api/[controller]")]
-	[ApiController]
-	public class BaseController : ControllerBase
+	public IActionResult Content(object obj)
 	{
-		public IActionResult Content(object obj)
-		{
-			return Content(obj.ToJson(), "application/json");
-		}
+		return Content(obj.ToJson(), "application/json");
 	}
 }
