739b6eee76ef5e73043bdd04effcc2680270cba9
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-09 00:06:16 +0630

    Format Code

--- DotNet8.Architectures.ModularMonolithic.Modules.Application/Features/Blog/GetBlogList/GetBlogListQueryHandler.cs
+++ DotNet8.Architectures.ModularMonolithic.Modules.Application/Features/Blog/GetBlogList/GetBlogListQueryHandler.cs
@@ -1,6 +1,5 @@
-﻿namespace DotNet8.Architectures.ModularMonolithic.Modules.Application.Features.Blog.GetBlogList
+﻿namespace DotNet8.Architectures.ModularMonolithic.Modules.Application.Features.Blog.GetBlogList;
+
+internal class GetBlogListQueryHandler
 {
-	internal class GetBlogListQueryHandler
-	{
-	}
 }
