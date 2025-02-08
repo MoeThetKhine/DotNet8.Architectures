fe1a27ad2d6fefc998a4c4375e74b5c5f32ab7d7
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-08 23:46:51 +0630

    -

--- DotNet8.Architectures.ModularMonolithic.Modules.Application/Features/Blog/UpdateBlog/UpdateBlogCommandHandler.cs
+++ DotNet8.Architectures.ModularMonolithic.Modules.Application/Features/Blog/UpdateBlog/UpdateBlogCommandHandler.cs
@@ -1,5 +1,5 @@
 ﻿namespace DotNet8.Architectures.ModularMonolithic.Modules.Application.Features.Blog.UpdateBlog;
 
-internal class UpdateBlogCommandHandler
+public class UpdateBlogCommandHandler
 {
 }
