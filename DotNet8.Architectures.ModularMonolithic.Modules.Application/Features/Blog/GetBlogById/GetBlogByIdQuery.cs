43c1cd1639dbc47089af96d5ab879d1e8c18b22c
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-09 00:12:09 +0630

    Format Code

--- DotNet8.Architectures.ModularMonolithic.Modules.Application/Features/Blog/GetBlogById/GetBlogByIdQuery.cs
+++ DotNet8.Architectures.ModularMonolithic.Modules.Application/Features/Blog/GetBlogById/GetBlogByIdQuery.cs
@@ -1,6 +1,5 @@
-﻿namespace DotNet8.Architectures.ModularMonolithic.Modules.Application.Features.Blog.GetBlogById
+﻿namespace DotNet8.Architectures.ModularMonolithic.Modules.Application.Features.Blog.GetBlogById;
+
+internal class GetBlogByIdQuery
 {
-	internal class GetBlogByIdQuery
-	{
-	}
 }
