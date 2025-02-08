83ed4596c673efad5de4657ee669d8e85fb8e9fd
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-08 21:26:14 +0630

    Feat : Add Global Using

--- DotNet8.Architectures.ModularMonolithic.Modules.Presentation/Extensions/DependencyInjection.cs
+++ DotNet8.Architectures.ModularMonolithic.Modules.Presentation/Extensions/DependencyInjection.cs
@@ -1,9 +1,4 @@
-﻿using DotNet8.Architectures.Clean.Domain.Features.Blog;
-using DotNet8.Architectures.Clean.Infrastructure.Features.Blog;
-using DotNet8.Architectures.DbServices.Models;
-using Microsoft.EntityFrameworkCore;
-
-namespace DotNet8.Architectures.ModularMonolithic.Modules.Presentation.Extensions;
+﻿namespace DotNet8.Architectures.ModularMonolithic.Modules.Presentation.Extensions;
 
 public static class DependencyInjection
 {
--- DotNet8.Architectures.ModularMonolithic.Modules.Presentation/GlobalUsing.cs
+++ DotNet8.Architectures.ModularMonolithic.Modules.Presentation/GlobalUsing.cs
@@ -1,2 +1,6 @@
 ﻿global using DotNet8.Architectures.Shared;
 global using Microsoft.AspNetCore.Mvc;
+global using DotNet8.Architectures.Clean.Domain.Features.Blog;
+global using DotNet8.Architectures.Clean.Infrastructure.Features.Blog;
+global using DotNet8.Architectures.DbServices.Models;
+global using Microsoft.EntityFrameworkCore;
