c3974ed70a741427083b2deaf044ad84182ab9d0
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-08 21:34:40 +0630

    -

--- DotNet8.Architectures.ModularMonolithic.Modules.Infrastructure/Features/Blog/BlogDbContext.cs
+++ DotNet8.Architectures.ModularMonolithic.Modules.Infrastructure/Features/Blog/BlogDbContext.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DotNet8.Architectures.ModularMonolithic.Modules.Infrastructure.Features.Blog
 {
-	internal class BlogDbContext
+	public class BlogDbContext
 	{
 	}
 }
