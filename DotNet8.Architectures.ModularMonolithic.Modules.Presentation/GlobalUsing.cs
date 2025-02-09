4caeed5bbfb1ea9e50573f3adc8f56ebfb5a8161
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-09 10:30:54 +0630

    -

--- DotNet8.Architectures.ModularMonolithic.Modules.Presentation/GlobalUsing.cs
+++ DotNet8.Architectures.ModularMonolithic.Modules.Presentation/GlobalUsing.cs
@@ -1,8 +1,5 @@
 ﻿global using DotNet8.Architectures.Shared;
 global using Microsoft.AspNetCore.Mvc;
-global using DotNet8.Architectures.Clean.Domain.Features.Blog;
-global using DotNet8.Architectures.Clean.Infrastructure.Features.Blog;
 global using DotNet8.Architectures.DbServices.Models;
 global using Microsoft.EntityFrameworkCore;
-global using DotNet8.Architectures.Hexgonal.Api.Extensions;
 
