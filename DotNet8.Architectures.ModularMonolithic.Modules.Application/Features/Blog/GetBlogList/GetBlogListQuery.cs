6f97d1b192de6d1abee7e2129d47d0a271a18f3a
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-09 10:39:59 +0630

    Feat : Add GetBlogListQuery

--- DotNet8.Architectures.ModularMonolithic.Modules.Application/Features/Blog/GetBlogList/GetBlogListQuery.cs
+++ DotNet8.Architectures.ModularMonolithic.Modules.Application/Features/Blog/GetBlogList/GetBlogListQuery.cs
@@ -1,5 +1,20 @@
-﻿namespace DotNet8.Architectures.ModularMonolithic.Modules.Application.Features.Blog.GetBlogList;
+﻿using DotNet8.Architectures.DTO.Features.Blog;
+using DotNet8.Architectures.Utils;
+using MediatR;
+using System.Net;
 
-public class GetBlogListQuery
+namespace DotNet8.Architectures.ModularMonolithic.Modules.Application.Features.Blog.GetBlogList;
+
+public class GetBlogListQuery : IRequest<Result<BlogListModelV1>>
 {
+	public int PageNo {  get; set; }
+
+	public int PageSize { get; set; }
+
+	public GetBlogListQuery(int pageNo, int pageSize)
+	{
+		PageNo = pageNo;
+		PageSize = pageSize;
+	}
 }
+
