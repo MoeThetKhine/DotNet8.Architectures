f4d38063eac82216fef1eb667837d77758877586
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-09 20:58:19 +0630

    Feat : Add Global Using

--- DotNet8.Architectures.ModularMonolithic.Modules.Presentation/GlobalUsing.cs
+++ DotNet8.Architectures.ModularMonolithic.Modules.Presentation/GlobalUsing.cs
@@ -4,5 +4,5 @@ global using DotNet8.Architectures.DbServices.Models;
 global using Microsoft.EntityFrameworkCore;
 global using DotNet8.Architectures.ModularMonolithic.Modules.Presentation.Extensions;
 global using DotNet8.Architectures.ModularMonolithic.Modules.Application.Extensions;
-
-
+global using DotNet8.Architectures.ModularMonolithic.Modules.Application.Features.Blog.GetBlogList;
+global using MediatR;
