781321794a3bd1a6b6b3e0cfa4c96ba48c6f3d71
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-09 11:41:23 +0630

    Feat : Add Global Using

--- DotNet8.Architectures.ModularMonolithic.Modules.Application/Features/Blog/GetBlogList/GetBlogListQueryHandler.cs
+++ DotNet8.Architectures.ModularMonolithic.Modules.Application/Features/Blog/GetBlogList/GetBlogListQueryHandler.cs
@@ -1,7 +1,4 @@
-﻿using DotNet8.Architectures.ModularMonolithic.Modules.Domain.Features.Blog;
-using DotNet8.Architectures.Utils.Resources;
-
-namespace DotNet8.Architectures.ModularMonolithic.Modules.Application.Features.Blog.GetBlogList;
+﻿namespace DotNet8.Architectures.ModularMonolithic.Modules.Application.Features.Blog.GetBlogList;
 
 public class GetBlogListQueryHandler : IRequestHandler<GetBlogListQuery, Result<BlogListModelV1>>
 {
--- DotNet8.Architectures.ModularMonolithic.Modules.Application/GlobalUsing.cs
+++ DotNet8.Architectures.ModularMonolithic.Modules.Application/GlobalUsing.cs
@@ -2,3 +2,5 @@
 global using DotNet8.Architectures.DTO.Features.Blog;
 global using DotNet8.Architectures.Utils;
 global using MediatR;
+global using DotNet8.Architectures.ModularMonolithic.Modules.Domain.Features.Blog;
+global using DotNet8.Architectures.Utils.Resources;
