83b54a151bf50b73fc20130fb7eac8b7465a7286
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-11 16:14:14 +0630

    Feat : Add GetBlogByIdAsync in IBlogRepository

--- DotNet8.Architectures.ModularMonolithic.Modules.Domain/Features/Blog/IBlogRepository.cs
+++ DotNet8.Architectures.ModularMonolithic.Modules.Domain/Features/Blog/IBlogRepository.cs
@@ -5,6 +5,8 @@
 public interface IBlogRepository
 {
 	Task<Result<BlogListModelV1>> GetBlogsAsync(int pageNo, int pageSize, CancellationToken cancellationToken);
+
+	Task<Result<BlogModel>> GetBlogByIdAsync(int id, CancellationToken cancellationToken);
 }
 
 #endregion
