dc50253d7e0a5058aee0cc79d41a7a7d9ddf8499
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-11 22:00:44 +0630

    Feat : Add Global Using

--- DotNet8.Architectures.ModularMonolithic.Modules.Application/Features/Blog/CreateBlog/CreateBlogCommandHandler.cs
+++ DotNet8.Architectures.ModularMonolithic.Modules.Application/Features/Blog/CreateBlog/CreateBlogCommandHandler.cs
@@ -1,6 +1,4 @@
-﻿using DotNet8.Architectures.Shared;
-
-namespace DotNet8.Architectures.ModularMonolithic.Modules.Application.Features.Blog.CreateBlog;
+﻿namespace DotNet8.Architectures.ModularMonolithic.Modules.Application.Features.Blog.CreateBlog;
 
 public class CreateBlogCommandHandler : IRequestHandler<CreateBlogCommand, Result<BlogModel>>
 {
