8a24929443923fb81d1b355b002bab2857e2e933
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-12 04:15:06 +0630

    Feat : Add UpdateBlogCommand

--- DotNet8.Architectures.ModularMonolithic.Modules.Application/Features/Blog/UpdateBlog/UpdateBlogCommand.cs
+++ DotNet8.Architectures.ModularMonolithic.Modules.Application/Features/Blog/UpdateBlog/UpdateBlogCommand.cs
@@ -1,5 +1,14 @@
 ﻿namespace DotNet8.Architectures.ModularMonolithic.Modules.Application.Features.Blog.UpdateBlog;
 
-public class UpdateBlogCommand
+public class UpdateBlogCommand : IRequest<Result<BlogModel>>
 {
+	public BlogRequestModel RequestModel { get; set; }
+
+	public int BlogId {  get; set; }
+
+	public UpdateBlogCommand(BlogRequestModel requestModel, int blogId)
+	{
+		RequestModel = requestModel;
+		BlogId = blogId;
+	}
 }
