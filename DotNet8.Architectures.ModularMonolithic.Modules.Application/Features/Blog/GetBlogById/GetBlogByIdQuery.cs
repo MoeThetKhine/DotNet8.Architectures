cb0351c0d6ea851f6b9a598c500311d48e857adc
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-11 16:30:53 +0630

    -

--- DotNet8.Architectures.ModularMonolithic.Modules.Application/Features/Blog/GetBlogById/GetBlogByIdQuery.cs
+++ DotNet8.Architectures.ModularMonolithic.Modules.Application/Features/Blog/GetBlogById/GetBlogByIdQuery.cs
@@ -1,5 +1,7 @@
 ﻿namespace DotNet8.Architectures.ModularMonolithic.Modules.Application.Features.Blog.GetBlogById;
 
+#region GetBlogByIdQuery
+
 public class GetBlogByIdQuery : IRequest<Result<BlogModel>>
 {
 	public int BlogId {  get; set; }
@@ -9,3 +11,5 @@ public class GetBlogByIdQuery : IRequest<Result<BlogModel>>
 		BlogId = blogId;
 	}
 }
+
+#endregion
