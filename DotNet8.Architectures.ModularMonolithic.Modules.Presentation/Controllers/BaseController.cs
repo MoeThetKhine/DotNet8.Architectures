a2ed4e78794ddd7109bc38b07038cb6cb1123fc5
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-11 16:12:32 +0630

    Feat : Add Content in BaseController

--- DotNet8.Architectures.ModularMonolithic.Modules.Presentation/Controllers/BaseController.cs
+++ DotNet8.Architectures.ModularMonolithic.Modules.Presentation/Controllers/BaseController.cs
@@ -7,7 +7,7 @@ public class BaseController : ControllerBase
 
 	#region Content
 
-	public IActionResult Content(object obj)
+	protected IActionResult Content(object obj)
 	{
 		return Content(obj.ToJson(), "application/json");
 	}
