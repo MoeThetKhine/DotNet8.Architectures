8022f159ae9c7036dff8481421fd351e26752bf2
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-12 22:17:45 +0630

    -

--- DotNet8.Architectures.ModularMonolithic.Modules.Presentation/Controllers/Blog/BlogController.cs
+++ DotNet8.Architectures.ModularMonolithic.Modules.Presentation/Controllers/Blog/BlogController.cs
@@ -66,6 +66,8 @@ public class BlogController : BaseController
 
 	#endregion
 
+	#region PatchBlog
+
 	[HttpPatch("{id}")]
 	public async Task<IActionResult> PatchBlog([FromBody] BlogRequestModel requestModel, int id, CancellationToken cancellationToken)
 	{
@@ -73,6 +75,8 @@ public class BlogController : BaseController
 		var result = await _mediator.Send(command, cancellationToken);
 		return Content(result);
 	}
+
+	#endregion
 
 	[HttpDelete("{id}")]
 	public async Task<IActionResult> DeleteBlog(int id, CancellationToken cancellationToken)
