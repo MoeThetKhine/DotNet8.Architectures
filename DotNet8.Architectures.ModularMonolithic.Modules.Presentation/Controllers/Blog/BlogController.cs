fc0dbd6f6c22a14f4130d56487f7b27a11774a09
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-12 15:25:08 +0630

    Feat : Add UpdateBlog in Controller

--- DotNet8.Architectures.ModularMonolithic.Modules.Presentation/Controllers/Blog/BlogController.cs
+++ DotNet8.Architectures.ModularMonolithic.Modules.Presentation/Controllers/Blog/BlogController.cs
@@ -1,4 +1,6 @@
-﻿namespace DotNet8.Architectures.ModularMonolithic.Modules.Presentation.Controllers.Blog;
+﻿using DotNet8.Architectures.ModularMonolithic.Modules.Application.Features.Blog.UpdateBlog;
+
+namespace DotNet8.Architectures.ModularMonolithic.Modules.Presentation.Controllers.Blog;
 
 [Route("api/[controller]")]
 [ApiController]
@@ -49,5 +51,14 @@ public class BlogController : BaseController
 	}
 
 	#endregion
+
+	[HttpPut("{id}")]
+	public async Task<IActionResult> UpdateBlog([FromBody] BlogRequestModel requestModel, int id , CancellationToken cancellationToken)
+	{
+		var command = new UpdateBlogCommand(requestModel, id);
+		var result = await _mediator.Send(command,cancellationToken);
+
+		return Content(result);
+	}
 
 }
