76ffb83a39588e02a2abce4715fe020554de17b2
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-12 15:24:45 +0630

    Feat : Add UpdateBlogCommandHandler

--- DotNet8.Architectures.ModularMonolithic.Modules.Application/Features/Blog/UpdateBlog/UpdateBlogCommandHandler.cs
+++ DotNet8.Architectures.ModularMonolithic.Modules.Application/Features/Blog/UpdateBlog/UpdateBlogCommandHandler.cs
@@ -1,5 +1,39 @@
 ﻿namespace DotNet8.Architectures.ModularMonolithic.Modules.Application.Features.Blog.UpdateBlog;
 
-public class UpdateBlogCommandHandler
+public class UpdateBlogCommandHandler : IRequestHandler<UpdateBlogCommand, Result<BlogModel>>
 {
+	private readonly IBlogRepository _blogRepository;
+
+	public UpdateBlogCommandHandler(IBlogRepository blogRepository)
+	{
+		_blogRepository = blogRepository;
+	}
+
+	public async Task<Result<BlogModel>> Handle(UpdateBlogCommand request, CancellationToken cancellationToken)
+	{
+		Result<BlogModel> result;
+
+		if(request.RequestModel.BlogTitle.IsNullOrEmpty())
+		{
+			result = Result<BlogModel>.Fail("Blog Title cannot be empty.");
+			goto result;
+		}
+
+		if(request.RequestModel.BlogAuthor.IsNullOrEmpty())
+		{
+			result = Result<BlogModel>.Fail("Blog Author cannot be empty.");
+			goto result;
+		}
+
+		if(request.RequestModel.BlogContent.IsNullOrEmpty())
+		{
+			result = Result<BlogModel>.Fail("Blog Content cannot be empty.");
+			goto result;
+		}
+
+		result = await _blogRepository.UpdateBlogAsync(request.RequestModel,request.BlogId,cancellationToken);
+
+		result:
+		return result;
+	}
 }
