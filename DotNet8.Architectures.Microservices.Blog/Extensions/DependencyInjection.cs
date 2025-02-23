defef917c90bd92ad1b6aea2e0b0e50aac2b1cb3
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-23 19:36:31 +0630

    -

--- DotNet8.Architectures.Microservices.Blog/Extensions/DependencyInjection.cs
+++ DotNet8.Architectures.Microservices.Blog/Extensions/DependencyInjection.cs
@@ -6,13 +6,18 @@ namespace DotNet8.Architectures.Microservices.Blog.Extensions
 {
 	public static class DependencyInjection
 	{
+
+		#region AddDependencyInjection
+
 		public static IServiceCollection AddDependencyInjection(this IServiceCollection services , WebApplicationBuilder builder)
 		{
 			return services
 				.AddDbContextService(builder)
 				.AddDataAccessService()
 				.AddBusinessLogicService();
-		} 
+		}
+
+		#endregion
 
 		private static IServiceCollection AddDbContextService(this IServiceCollection services, WebApplicationBuilder builder)
 		{
