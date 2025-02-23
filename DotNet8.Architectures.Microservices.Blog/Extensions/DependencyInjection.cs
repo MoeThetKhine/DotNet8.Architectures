4543c5d5d9142b26cd7dd176e150345a3df89921
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-23 19:58:51 +0630

    Format Code

--- DotNet8.Architectures.Microservices.Blog/Extensions/DependencyInjection.cs
+++ DotNet8.Architectures.Microservices.Blog/Extensions/DependencyInjection.cs
@@ -2,57 +2,56 @@
 using DotNet8.Architectures.Microservices.Blog.Features.Blog;
 using Microsoft.EntityFrameworkCore;
 
-namespace DotNet8.Architectures.Microservices.Blog.Extensions
+namespace DotNet8.Architectures.Microservices.Blog.Extensions;
+
+public static class DependencyInjection
 {
-	public static class DependencyInjection
+
+	#region AddDependencyInjection
+
+	public static IServiceCollection AddDependencyInjection(this IServiceCollection services , WebApplicationBuilder builder)
 	{
-
-		#region AddDependencyInjection
-
-		public static IServiceCollection AddDependencyInjection(this IServiceCollection services , WebApplicationBuilder builder)
-		{
-			return services
-				.AddDbContextService(builder)
-				.AddDataAccessService()
-				.AddBusinessLogicService();
-		}
-
-		#endregion
-
-		#region AddDbContextService
-
-		private static IServiceCollection AddDbContextService(this IServiceCollection services, WebApplicationBuilder builder)
-		{
-			builder.Services.AddDbContext<AppDbContext>(
-				opt =>
-				{
-					opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
-					opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
-				},
-				ServiceLifetime.Transient,
-				ServiceLifetime.Transient
-				);
-			return services;
-		}
-
-		#endregion
-
-		#region AddDataAccessService
-
-		private static IServiceCollection AddDataAccessService(this  IServiceCollection services)
-		{
-			return services.AddScoped<DA_Blog>();
-		}
-
-		#endregion
-
-		#region AddBusinessLogicService
-
-		private static IServiceCollection AddBusinessLogicService(this IServiceCollection services)
-		{
-			return services.AddScoped<BL_Blog>();
-		}
-
-		#endregion
+		return services
+			.AddDbContextService(builder)
+			.AddDataAccessService()
+			.AddBusinessLogicService();
 	}
+
+	#endregion
+
+	#region AddDbContextService
+
+	private static IServiceCollection AddDbContextService(this IServiceCollection services, WebApplicationBuilder builder)
+	{
+		builder.Services.AddDbContext<AppDbContext>(
+			opt =>
+			{
+				opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+				opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+			},
+			ServiceLifetime.Transient,
+			ServiceLifetime.Transient
+			);
+		return services;
+	}
+
+	#endregion
+
+	#region AddDataAccessService
+
+	private static IServiceCollection AddDataAccessService(this  IServiceCollection services)
+	{
+		return services.AddScoped<DA_Blog>();
+	}
+
+	#endregion
+
+	#region AddBusinessLogicService
+
+	private static IServiceCollection AddBusinessLogicService(this IServiceCollection services)
+	{
+		return services.AddScoped<BL_Blog>();
+	}
+
+	#endregion
 }
