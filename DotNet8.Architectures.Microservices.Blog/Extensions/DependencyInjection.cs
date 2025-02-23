76dbd02c9d42d2020c79a0f5f2babf98618b3605
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-23 19:30:08 +0630

    Feat : Add AddDbContextService

--- DotNet8.Architectures.Microservices.Blog/Extensions/DependencyInjection.cs
+++ DotNet8.Architectures.Microservices.Blog/Extensions/DependencyInjection.cs
@@ -1,6 +1,28 @@
-﻿namespace DotNet8.Architectures.Microservices.Blog.Extensions
+﻿using DotNet8.Architectures.DbServices.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNet8.Architectures.Microservices.Blog.Extensions
 {
-	public class DependencyInjection
+	public static class DependencyInjection
 	{
+		public static IServiceCollection AddDependencyInjection(this IServiceCollection services , WebApplicationBuilder builder)
+		{
+			return services
+
+		} 
+
+		private static IServiceCollection AddDbContextService(this IServiceCollection services, WebApplicationBuilder builder)
+		{
+			builder.Services.AddDbContext<AppDbContext>(
+				opt =>
+				{
+					opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+					opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+				},
+				ServiceLifetime.Transient,
+				ServiceLifetime.Transient
+				);
+			return services;
+		}
 	}
 }
