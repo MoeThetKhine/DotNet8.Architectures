1b05a27d034eb2c0f5ec7e381f777abae630bba0
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-24 19:26:11 +0630

    Format Code

--- DotNet8.Architectures.Microservices.Blog/Features/Blog/DA_Blog.cs
+++ DotNet8.Architectures.Microservices.Blog/Features/Blog/DA_Blog.cs
@@ -1,56 +1,55 @@
-﻿namespace DotNet8.Architectures.Microservices.Blog.Features.Blog
+﻿namespace DotNet8.Architectures.Microservices.Blog.Features.Blog;
+
+public class DA_Blog
 {
-	public class DA_Blog
+	private readonly AppDbContext _context;
+
+	public DA_Blog(AppDbContext context)
 	{
-		private readonly AppDbContext _context;
-
-		public DA_Blog(AppDbContext context)
-		{
-			_context = context;
-		}
-
-		#region GetBlogsAsync
-
-		public async Task<Result<BlogListModelV1>> GetBlogsAsync(int pageNo, int pageSize, CancellationToken cancellationToken)
-		{
-			Result<BlogListModelV1> result;
-
-			try
-			{
-				var query = _context.Tbl_Blogs.OrderByDescending(x => x.BlogId);
-				var lst = await query
-					.Paginate(pageNo, pageSize)
-					.ToListAsync(cancellationToken: cancellationToken);
-				var totalCount = await query.CountAsync(cancellationToken: cancellationToken);
-				var pageCount = totalCount / pageSize;
-
-				if(totalCount % pageSize > 0)
-				{
-					pageCount++;
-				}
-
-				var pageSettingModel = new PageSettingModel(pageNo, pageSize, pageCount, totalCount);
-				var model = new BlogListModelV1()
-				{
-					DataLst = lst.Select(x => new BlogModel()
-					{
-						BlogId = x.BlogId,
-						BlogTitle = x.BlogTitle,
-						BlogAuthor = x.BlogAuthor,
-						BlogContent = x.BlogContent,
-					}).AsQueryable(),
-					PageSetting = pageSettingModel
-				};
-
-				result = Result<BlogListModelV1>.Success(model);
-			}
-			catch (Exception ex)
-			{
-				result = Result<BlogListModelV1>.Failure(ex);
-			}
-			return result;
-		}
-
-		#endregion
+		_context = context;
 	}
+
+	#region GetBlogsAsync
+
+	public async Task<Result<BlogListModelV1>> GetBlogsAsync(int pageNo, int pageSize, CancellationToken cancellationToken)
+	{
+		Result<BlogListModelV1> result;
+
+		try
+		{
+			var query = _context.Tbl_Blogs.OrderByDescending(x => x.BlogId);
+			var lst = await query
+				.Paginate(pageNo, pageSize)
+				.ToListAsync(cancellationToken: cancellationToken);
+			var totalCount = await query.CountAsync(cancellationToken: cancellationToken);
+			var pageCount = totalCount / pageSize;
+
+			if(totalCount % pageSize > 0)
+			{
+				pageCount++;
+			}
+
+			var pageSettingModel = new PageSettingModel(pageNo, pageSize, pageCount, totalCount);
+			var model = new BlogListModelV1()
+			{
+				DataLst = lst.Select(x => new BlogModel()
+				{
+					BlogId = x.BlogId,
+					BlogTitle = x.BlogTitle,
+					BlogAuthor = x.BlogAuthor,
+					BlogContent = x.BlogContent,
+				}).AsQueryable(),
+				PageSetting = pageSettingModel
+			};
+
+			result = Result<BlogListModelV1>.Success(model);
+		}
+		catch (Exception ex)
+		{
+			result = Result<BlogListModelV1>.Failure(ex);
+		}
+		return result;
+	}
+
+	#endregion
 }
