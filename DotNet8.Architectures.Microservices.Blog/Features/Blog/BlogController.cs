60b69db929e0cd70e22b76be2a4daddfcff48dc1
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-24 19:33:18 +0630

    -

--- DotNet8.Architectures.Microservices.Blog/Features/Blog/BlogController.cs
+++ DotNet8.Architectures.Microservices.Blog/Features/Blog/BlogController.cs
@@ -6,5 +6,13 @@ namespace DotNet8.Architectures.Microservices.Blog.Features.Blog
 	[ApiController]
 	public class BlogController : ControllerBase
 	{
+		private readonly DA_Blog _dA_Blog;
+
+		public BlogController(DA_Blog dA_Blog)
+		{
+			_dA_Blog = dA_Blog;
+		}
+
+
 	}
 }
