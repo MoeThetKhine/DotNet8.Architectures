ea56416f04e563664050d1473601dede04c2e4bd
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-24 19:04:42 +0630

    -

--- DotNet8.Architectures.Microservices.Blog/Features/Blog/DA_Blog.cs
+++ DotNet8.Architectures.Microservices.Blog/Features/Blog/DA_Blog.cs
@@ -1,6 +1,4 @@
-﻿using DotNet8.Architectures.DbServices.Models;
-
-namespace DotNet8.Architectures.Microservices.Blog.Features.Blog
+﻿namespace DotNet8.Architectures.Microservices.Blog.Features.Blog
 {
 	public class DA_Blog
 	{
