2b947acbf037bef9bedc5ceb8ab0888aab99a0b2
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-24 19:00:48 +0630

    Feat : Add AddDependencyInjection

--- DotNet8.Architectures.Microservices.Blog/Program.cs
+++ DotNet8.Architectures.Microservices.Blog/Program.cs
@@ -1,8 +1,12 @@
+using DotNet8.Architectures.Microservices.Blog.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDependencyInjection(builder);
 
 var app = builder.Build();
 
